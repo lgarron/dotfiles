9f5556384433271cfac59ad608d74948f7adb359
Lucas Garron <code@garron.net>
2014-07-19 19:04:42 -0700

    Mathematica Copy: return copied text from function.

--- home/Library/Mathematica/Kernel/init.m
+++ home/Library/Mathematica/Kernel/init.m
@@ -4,5 +4,6 @@ Copy[text_] := With[
   {tempClipboardFile = FileNameJoin[{$TemporaryDirectory, "mathematica_clipboard_temp.txt"}]},
   Export[tempClipboardFile, text, "text"];
   Run["cat " <> tempClipboardFile <> " | pbcopy"];
-  DeleteFile[tempClipboardFile]
+  DeleteFile[tempClipboardFile];
+  text
 ]

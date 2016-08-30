2b72bd2feee04544b409687e6c3270d50e1f1075
Lucas Garron <code@garron.net>
2016-08-30 11:42:13 -0700

    [chrome.fish] Move git completion hacks to a separate file.
    Suggested by @faho at https://github.com/fish-shell/fish-shell/issues/3342#issuecomment-243416340

--- dotfiles/chrome/.config/fish/chrome.fish
+++ dotfiles/chrome/.config/fish/chrome.fish
@@ -317,64 +317,6 @@ debug_devtools = true
     end
 
 
-## Git
-
-    function in_chromium_repo
-      if not git rev-parse ^ /dev/null
-        false
-      else if [ (git remote get-url origin) = "https://chromium.googlesource.com/chromium/src.git" ]
-        true
-      else
-        false
-      end
-    end
-
-    # Stub out some slow git completions.
-    function __fish_git_add_files; end
-    function __fish_git_commits; end
-    function __fish_git_modified_files; end
-    function __fish_git_unique_remote_branches; end
-
-    function __fish_git_tags
-      if [ in_chromium_repo ]
-        # Ignore thousands of release tags.
-
-        # I currently don't use tags. Can save a bunch of extra time by ignoring.
-        # command git tag | grep -v "^\d\+\.\d\+\.\d\+\.\d\+\$"
-      else
-        command git tag
-      end
-    end
-
-    function __fish_git_modified_files
-      if [ in_chromium_repo ]
-        # *sigh*
-      else
-        # git diff --name-only hands us filenames relative to the git toplevel
-        set -l root (command git rev-parse --show-toplevel)
-        # Print files from the current $PWD as-is, prepend all others with ":/" (relative to toplevel in git-speak)
-        # This is a bit simplistic but finding the lowest common directory and then replacing everything else in $PWD with ".." is a bit annoying
-        string replace -- "$PWD/" "" "$root/"(command git diff --name-only ^/dev/null) | string replace "$root/" ":/"
-      end
-    end
-
-    function __fish_git_branches
-      if [ in_chromium_repo ]
-        # Ignore a whole bunch of release branches.
-        command git branch --no-color ^/dev/null | grep -v ' -> ' | sed -e 's/^..//' -e 's/^remotes\///'
-      else
-        command git branch --no-color -a ^/dev/null | grep -v ' -> ' | sed -e 's/^..//' -e 's/^remotes\///'
-      end
-    end
-
-    function __fish_git_commits
-      if [ in_chromium_repo ]
-      else
-        command git log --pretty=tformat:"%h"\t"%s" --all ^/dev/null \
-        | string replace -r '(.{73}).+' '$1…'
-      end
-    end
-
 ### "GitHub"
 
     function switch-chrome-user
---
+++ dotfiles/chrome/.config/fish/completions/git.fish
@@ -0,0 +1,61 @@
+source $__fish_datadir/completions/git.fish
+
+### Git completion hacks for Chromium
+
+    function in_chromium_repo
+      if not git rev-parse ^ /dev/null
+        false
+      else if [ (git remote get-url origin) = "https://chromium.googlesource.com/chromium/src.git" ]
+        true
+      else
+        false
+      end
+    end
+
+    function __fish_git_tags
+      if [ in_chromium_repo ]
+        # I currently don't use tags. Save a bunch of extra time by ignoring.
+        # Uncomment the line below to list tags but ignore thousands of release tags.
+        # command git tag | grep -v "^\d\+\.\d\+\.\d\+\.\d\+\$"
+      else
+        command git tag
+      end
+    end
+
+    function __fish_git_modified_files
+      if [ in_chromium_repo ]
+        # sloooooow. untracked-cache doesn't seem to work.
+      else
+        # git diff --name-only hands us filenames relative to the git toplevel
+        set -l root (command git rev-parse --show-toplevel)
+        # Print files from the current $PWD as-is, prepend all others with ":/" (relative to toplevel in git-speak)
+        # This is a bit simplistic but finding the lowest common directory and then replacing everything else in $PWD with ".." is a bit annoying
+        string replace -- "$PWD/" "" "$root/"(command git diff --name-only ^/dev/null) | string replace "$root/" ":/"
+      end
+    end
+
+    function __fish_git_branches
+      if [ in_chromium_repo ]
+        # Ignore a whole bunch of release branches.
+        command git branch --no-color ^/dev/null | grep -v ' -> ' | sed -e 's/^..//' -e 's/^remotes\///'
+      else
+        command git branch --no-color -a ^/dev/null | grep -v ' -> ' | sed -e 's/^..//' -e 's/^remotes\///'
+      end
+    end
+
+    function __fish_git_unique_remote_branches
+      if [ in_chromium_repo ]
+        # Remote branches are useful sometimes, but let's cut them out for performance.
+      else
+        command git branch --no-color -a $argv ^/dev/null | string match -r -v ' -> ' | string trim -c "* " | string replace -r "^remotes/[^/]*/" "" | sort | uniq -u
+      end
+    end
+
+    function __fish_git_commits
+      if [ in_chromium_repo ] 
+        # Performance is atrocious if we list all Chromium commit hashes.
+      else
+        command git log --pretty=tformat:"%h"\t"%s" --all ^/dev/null \
+        | string replace -r '(.{73}).+' '$1…'
+      end
+    end

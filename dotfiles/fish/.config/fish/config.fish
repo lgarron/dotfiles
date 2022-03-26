503211afcb43fbdba91ca070afdd3390d0dfc21b
Lucas Garron <code@garron.net>
2022-03-25 20:54:18 -0700

    [fish] Add abbreviation: `r` for `open -R`.

--- dotfiles/fish/.config/fish/config.fish
+++ dotfiles/fish/.config/fish/config.fish
@@ -223,6 +223,8 @@
         end
     end
 
+    abbr -a r "open -R"
+
     # set STDERRED_PATH "$HOME/Code/git/github.com/sickill/stderred/builda/libtest_stderred.dylib"
     # if test -f "$STDERRED_PATH"
     #     set -x DYLD_INSERT_LIBRARIES "$STDERRED_PATH"

95a1e079093271e72bf477beea309548703e5d2b
Lucas Garron <code@garron.net>
2022-04-27 19:15:45 -0700

    [config.fish] Add Linuxbrew paths.

--- dotfiles/fish/.config/fish/config.fish
+++ dotfiles/fish/.config/fish/config.fish
@@ -2,7 +2,16 @@
 
 # Setup
 
-    set PATH $PATH /opt/homebrew/bin
+    function add_to_path_if_exists
+      set NEW_PATH_COMPONENT $argv[1]
+      if test -d $NEW_PATH_COMPONENT
+        set PATH $PATH $NEW_PATH_COMPONENT
+      end
+    end
+
+    add_to_path_if_exists /opt/homebrew/bin # macOS (Apple Silicon)
+    add_to_path_if_exists /home/linuxbrew/.linuxbrew/bin # for codespaces
+    add_to_path_if_exists /home/linuxbrew/.linuxbrew/sbin # for codespaces
 
     # Only print if we're a TTY.
     # This prevents `protocol error: bad line length character` in git.

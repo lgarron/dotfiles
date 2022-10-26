decd7b56843537f810b6b599274ccfab41787eed
Lucas Garron <code@garron.net>
2022-10-26 10:11:53 -0700

    [fish] Split dev commands into a separate file.

--- dotfiles/fish/.config/fish/config.fish
+++ dotfiles/fish/.config/fish/config.fish
@@ -172,22 +172,6 @@
         cd $argv[1]
     end
 
-### `node`
-
-    abbr -a niy "npm init -y"
-    abbr -a ni "npm install;"
-    abbr -a nis "npm install --save"
-    abbr -a nid "npm install --save-dev"
-    abbr -a nu "npm uninstall"
-    abbr -a nrd "npm run dev"
-    abbr -a rmnm "rm -rf ./node_modules ; and npm install"
-    abbr -a wtr "npx web-test-runner"
-
-    function bump-version
-      # /usr/bin/env echo -n v
-      npm version --no-git-tag-version $argv[1]
-    end
-
 ### Editors
 
     # TODO: https://github.com/microsoft/vscode/issues/139634
@@ -197,16 +181,6 @@
 
     abbr -a c "code"
     abbr -a xc "xargs code"
-
-    abbr -a m "make"
-    abbr -a mb "make build"
-    abbr -a mc "make clean"
-    abbr -a md "make dev"
-    abbr -a ml "make deploy"
-    abbr -a mp "make publish"
-    abbr -a mt "make test"
-    abbr -a mcd "make clean dev"
-    abbr -a mcb "make clean build"
 
 ## Search
 
@@ -245,9 +219,6 @@
     abbr -a "sha256" "openssl dgst -sha256"
     abbr -a "sha512" "openssl dgst -sha512"
 
-    abbr -a "yarn" "npx yarn"
-    set -xg "EXPERIMENTAL_CUBING_JS_RELOAD_CHROME_MACOS" "1"
-
 ## Keyboard
 
     # https://developer.apple.com/library/content/technotes/tn2450/_index.html
@@ -267,11 +238,6 @@
     function remap-keys-show
         hidutil property --get "UserKeyMapping"
     end
-
-## Web
-
-    # *h*ttps *h*eaders
-    abbr -a "hh" "https -ph"
 
 # Shortcuts
 
@@ -342,6 +308,7 @@
 # Includes
 
     load_if_exists "git" $HOME/.config/fish/git.fish
+    load_if_exists "dev" $HOME/.config/fish/dev.fish
     load_if_exists "go" $HOME/.config/fish/go.fish
 
     loading_indicator ""
---
+++ dotfiles/fish/.config/fish/dev.fish
@@ -0,0 +1,35 @@
+## `dev`
+
+    abbr -a m "make"
+    abbr -a mb "make build"
+    abbr -a mc "make clean"
+    abbr -a md "make dev"
+    abbr -a ml "make deploy"
+    abbr -a mp "make publish"
+    abbr -a mt "make test"
+    abbr -a mcd "make clean dev"
+    abbr -a mcb "make clean build"
+
+## `node`
+
+    abbr -a niy "npm init -y"
+    abbr -a ni "npm install;"
+    abbr -a nis "npm install --save"
+    abbr -a nid "npm install --save-dev"
+    abbr -a nu "npm uninstall"
+    abbr -a nrd "npm run dev"
+    abbr -a rmnm "rm -rf ./node_modules ; and npm install"
+    abbr -a wtr "npx web-test-runner"
+
+    function bump-version
+      # /usr/bin/env echo -n v
+      npm version --no-git-tag-version $argv[1]
+    end
+
+    abbr -a "yarn" "npx yarn"
+    set -xg "EXPERIMENTAL_CUBING_JS_RELOAD_CHROME_MACOS" "1"
+
+## Web
+
+    # *h*ttps *h*eaders
+    abbr -a "hh" "https -ph"

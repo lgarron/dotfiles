a781a10ce6765c75053aad072b9560e21b2cce4e
Lucas Garron <code@garron.net>
2022-11-15 19:15:20 -0800

    [dev.fish] Add `ml` for `make lint`.

--- dotfiles/fish/.config/fish/dev.fish
+++ dotfiles/fish/.config/fish/dev.fish
@@ -5,11 +5,9 @@
     abbr -a mc "make clean"
     abbr -a md "make dev"
     abbr -a mdp "make deploy"
-    function ml
-        echo "Use: mdp"
-    end
     abbr -a mp "make publish"
     abbr -a mt "make test"
+    abbr -a ml "make lint"
     abbr -a mf "make format"
     abbr -a mcd "make clean dev"
     abbr -a mcb "make clean build"

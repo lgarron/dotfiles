1f82b800196d567d8f478a83a38ab20d9305b322
Lucas Garron <code@garron.net>
2023-01-26 01:41:39 -0800

    [fish] Change XDG workaround env vars to universal (automatically persisting across restart).

--- dotfiles/xdg-basedir-workarounds/.config/fish/xdg-basedir-workarounds.fish
+++ dotfiles/xdg-basedir-workarounds/.config/fish/xdg-basedir-workarounds.fish
@@ -6,21 +6,21 @@
 
     # Explicitly set env vars to the default paths, since that will occasionally
     # get programs to behave.
-    set -xg XDG_CACHE_HOME $HOME/.cache
-    set -xg XDG_CONFIG_HOME $HOME/.config
-    set -xg XDG_DATA_HOME $HOME/.data
+    set -Ux XDG_CACHE_HOME $HOME/.cache
+    set -Ux XDG_CONFIG_HOME $HOME/.config
+    set -Ux XDG_DATA_HOME $HOME/.data
 
 ## XDG workarounds for specific programs.
 
-    set -xg LESSHISTFILE $XDG_DATA_HOME/lesshst
+    set -Ux LESSHISTFILE $XDG_DATA_HOME/lesshst
 
     # GnPG
     # See `lglogin.fish` for `launchctl setenv`
-    set -xg GNUPGHOME $XDG_CONFIG_HOME/gnupg
+    set -Ux GNUPGHOME $XDG_CONFIG_HOME/gnupg
 
     # HTTPie
     # Workaround from https://github.com/jakubroztocil/httpie/issues/145#issuecomment-54704487
-    set -xg HTTPIE_CONFIG_DIR $XDG_CONFIG_HOME/httpie
+    set -Ux HTTPIE_CONFIG_DIR $XDG_CONFIG_HOME/httpie
 
     # bash
     # Folder is created in `lglogin.fish`
@@ -28,46 +28,46 @@
     # We'd use "$XDG_DATA_HOME/bash/history", but bash won't create the
     # intermediate folder and this prevents us from having to create it
     # manually.
-    set -xg HISTFILE $XDG_DATA_HOME/bash_history
+    set -Ux HISTFILE $XDG_DATA_HOME/bash_history
 
     # gem
-    set -xg GEM_HOME $XDG_DATA_HOME/gem
-    set -xg GEM_SPEC_CACHE $XDG_CACHE_HOME/gem
+    set -Ux GEM_HOME $XDG_DATA_HOME/gem
+    set -Ux GEM_SPEC_CACHE $XDG_CACHE_HOME/gem
 
     # npm
-    set -xg NPM_CONFIG_USERCONFIG $XDG_CONFIG_HOME/npm/config
-    set -xg npm_config_userconfig $XDG_CONFIG_HOME/npm/config
-    set -xg NPM_CONFIG_CACHE $XDG_CACHE_HOME/npm
+    set -Ux NPM_CONFIG_USERCONFIG $XDG_CONFIG_HOME/npm/config
+    set -Ux npm_config_userconfig $XDG_CONFIG_HOME/npm/config
+    set -Ux NPM_CONFIG_CACHE $XDG_CACHE_HOME/npm
 
     # Wine
-    set -xg WINEPREFIX $XDG_DATA_HOME/wine
+    set -Ux WINEPREFIX $XDG_DATA_HOME/wine
 
     # wget
-    set -xg WGETRC $XDG_CONFIG_HOME/wget/wgetrc
+    set -Ux WGETRC $XDG_CONFIG_HOME/wget/wgetrc
 
     # xauth
-    set -xg XAUTHORITY $XDG_CACHE_DIR/xauth/Xauthority
+    set -Ux XAUTHORITY $XDG_CACHE_DIR/xauth/Xauthority
 
     # Bundler (Ruby)
-    set -xg BUNDLE_USER_CONFIG $XDG_CONFIG_HOME/bundle
-    set -xg BUNDLE_USER_CACHE  $XDG_CACHE_HOME/bundle
-    set -xg BUNDLE_USER_PLUGIN $XDG_DATA_HOME/bundle
+    set -Ux BUNDLE_USER_CONFIG $XDG_CONFIG_HOME/bundle
+    set -Ux BUNDLE_USER_CACHE  $XDG_CACHE_HOME/bundle
+    set -Ux BUNDLE_USER_PLUGIN $XDG_DATA_HOME/bundle
 
     # node
-    set -xg NODE_REPL_HISTORY $XDG_DATA_HOME/node_repl_history
+    set -Ux NODE_REPL_HISTORY $XDG_DATA_HOME/node_repl_history
 
     # VSCode
-    # set -xg VSCODE_EXTENSIONS $XDG_DATA_HOME/vscode/extensions
+    # set -Ux VSCODE_EXTENSIONS $XDG_DATA_HOME/vscode/extensions
 
     # rbenv
     # https://github.com/rbenv/rbenv/issues/811
-    set -xg RBENV_ROOT $XDG_DATA_HOME/rbenv
+    set -Ux RBENV_ROOT $XDG_DATA_HOME/rbenv
 
     # zsh
-    set -xg ZDOTDIR $XDG_CONFIG_HOME/zsh
+    set -Ux ZDOTDIR $XDG_CONFIG_HOME/zsh
 
     # cargo (Rust)
-    set -xg CARGO_HOME $XDG_DATA_HOME/cargo
+    set -Ux CARGO_HOME $XDG_DATA_HOME/cargo
 
     # Docker
-    set -xg DOCKER_CONFIG $XDG_CONFIG_HOME/docker
+    set -Ux DOCKER_CONFIG $XDG_CONFIG_HOME/docker

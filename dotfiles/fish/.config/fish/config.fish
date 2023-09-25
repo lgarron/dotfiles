a16182a74d85634a72006cd2c44d5bd6e99c5168
Lucas Garron <code@garron.net>
2023-09-25 16:42:38 -0700

    [config.fish] Add `wih` and `wi h` for `brew install --HEAD`.

--- dotfiles/fish/.config/fish/config.fish
+++ dotfiles/fish/.config/fish/config.fish
@@ -404,11 +404,14 @@
 
     abbr -a w "brew"
     abbr -a wi "brew install"
+    abbr -a wih "brew install --HEAD"
     abbr -a wu "brew uninstall"
     abbr -a ws "brew search"
     function _abbr_brew_install_fn; _abbr_define_subcommand install brew i; end; abbr -a _abbr_brew_install --regex i --position anywhere --function _abbr_brew_install_fn
+    function _abbr_brew_install_HEAD_fn; _abbr_define_subcommand_arg "--HEAD" brew install; end; abbr -a _abbr_brew_install_HEAD --regex h --position anywhere --function _abbr_brew_install_HEAD_fn
     function _abbr_brew_uninstall_fn; _abbr_define_subcommand uninstall brew u; end; abbr -a _abbr_brew_uninstall --regex u --position anywhere --function _abbr_brew_uninstall_fn
     function _abbr_brew_search_fn; _abbr_define_subcommand search brew s; end; abbr -a _abbr_brew_search --regex s --position anywhere --function _abbr_brew_search_fn
+    
 
     abbr -a "md5" "openssl dgst -md5"
     abbr -a "sha1" "openssl dgst -sha1"

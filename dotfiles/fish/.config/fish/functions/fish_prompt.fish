c0fc2d5a8d31153efda7f1dda1ec9853f1450e4e
Lucas Garron <code@garron.net>
2024-10-09 15:59:53 -0700

    [fish_prompt.fish] Go all-in on LCARS.

--- dotfiles/fish/.config/fish/functions/fish_prompt.fish
+++ dotfiles/fish/.config/fish/functions/fish_prompt.fish
@@ -5,23 +5,10 @@ set __fish_git_prompt_show_informative_status 1
 
 set _FISH_LCARS_ORANGE F19E4C
 set _FISH_LCARS_LAVENDER B594E2
-set -l THEMES "html" "LCARS"
-function set_prompt_theme
-    set -g _FISH_PROMPT_THEME $argv[1]
-    if string match -e -- $argv[1] "html" > /dev/null
-        set -e ___fish_git_prompt_color_branch
-        set -e ___fish_git_prompt_color_branch_done
-        set -e ___fish_git_prompt_char_stateseparator
-    else
-        set -g ___fish_git_prompt_color_branch (set_color --reverse $_FISH_LCARS_ORANGE)" "
-        set -g ___fish_git_prompt_color_branch_done " "(set_color normal; set_color $_FISH_LCARS_ORANGE)
-        set -g ___fish_git_prompt_char_stateseparator " | "
-        # set ___fish_git_prompt_color_merging " "
-        # set ___fish_git_prompt_color_upstream " "
-    end
-end
 
-set_prompt_theme $THEMES[(random 1 (count $THEMES))]
+set -g ___fish_git_prompt_color_branch (set_color --reverse $_FISH_LCARS_ORANGE)" "
+set -g ___fish_git_prompt_color_branch_done " "(set_color normal; set_color $_FISH_LCARS_ORANGE)
+set -g ___fish_git_prompt_char_stateseparator " | "
 
 function _echo_padded
     set -l PREFIX $argv[1]
@@ -62,60 +49,42 @@ function fish_prompt --description 'Write out the prompt'
     # Write pipestatus
     set -l PREVIOUS_COMMAND_TIME "⏱️ "(math $CMD_DURATION / 1000)s
 
-    set -l _VCS ""
-    if string match -e -- $_FISH_PROMPT_THEME "html" > /dev/null
-        set -l prompt_status (__fish_print_pipestatus "[" "] " "|" (set_color $fish_color_status) (set_color --bold $fish_color_status) $last_pipestatus)
-
-        # html
-        if string match -e -- "$_FISH_PROMPT_AFTER_FIRST_RUN" true > /dev/null
-            echo -n (set_color blue)"</command>"(set_color purple)" <!-- $prompt_status"(set_color purple)$PREVIOUS_COMMAND_TIME" -->"
-        end
-        
-        echo (set_color blue)
-        echo -n "<command "
-        if string match -e "$EXPERIMENTAL_FISH_LAUNCHED" "true" > /dev/null
-            echo -n "experimental=\"🧪\" "
-        end
-        echo "path=\""(set_color $color_cwd)(pwd)(set_color blue)"\">"
-        set _VCS (fish_vcs_prompt)
-    else
-        # LCARS
-        if string match -e -- "$_FISH_PROMPT_AFTER_FIRST_RUN" true > /dev/null
-            set_color $_FISH_LCARS_LAVENDER
-            # echo (set_color $_FISH_LCARS_LAVENDER)"│"
-            set -l prompt_status (__fish_print_pipestatus "[" "] " "|" (set_color $_FISH_LCARS_LAVENDER) (set_color --bold red) $last_pipestatus)
-            if not string match -e -- "$prompt_status" " " > /dev/null
-                echo "├─ ❌ $prompt_status"(set_color $_FISH_LCARS_LAVENDER)"command status"
-            end
-            _echo_padded \
-                "╰─── $PREVIOUS_COMMAND_TIME " \
-                (set_color $_FISH_LCARS_ORANGE)
-            echo ""
-        end
-
-        set -l PREFIX_BEFORE_PWD (set_color $_FISH_LCARS_ORANGE)"╭─── "
-        if string match -e "$EXPERIMENTAL_FISH_LAUNCHED" "true" > /dev/null
-            set -l PREFIX_BEFORE_PWD "🐠🧪 "
-        end
-        set -l PREFIX $PREFIX_BEFORE_PWD(pwd)" "
-        if test (string length --visible $PREFIX) -gt $COLUMNS
-            set PREFIX $PREFIX_BEFORE_PWD(prompt_pwd)" "
+    # LCARS
+    if string match -e -- "$_FISH_PROMPT_AFTER_FIRST_RUN" true > /dev/null
+        set_color $_FISH_LCARS_LAVENDER
+        # echo (set_color $_FISH_LCARS_LAVENDER)"│"
+        set -l prompt_status (__fish_print_pipestatus "[" "] " "|" (set_color $_FISH_LCARS_LAVENDER) (set_color --bold red) $last_pipestatus)
+        if not string match -e -- "$prompt_status" " " > /dev/null
+            echo "├─ ❌ $prompt_status"(set_color $_FISH_LCARS_LAVENDER)"command status"
         end
         _echo_padded \
-            $PREFIX \
+            "╰─── $PREVIOUS_COMMAND_TIME " \
             (set_color $_FISH_LCARS_ORANGE)
-
-        set FISH_VCS_PROMPT (fish_vcs_prompt "%s")
-        if not string match -e "$FISH_VCS_PROMPT" "" > /dev/null
-            set -l PREFIX (set_color $_FISH_LCARS_ORANGE)"├─ "$FISH_VCS_PROMPT" "
-            echo $PREFIX
-        end
-        echo -n "├─ "
-        set suffix $suffix"
-" (set_color $_FISH_LCARS_ORANGE) "│"
+        echo ""
     end
+
+    set -l PREFIX_BEFORE_PWD (set_color $_FISH_LCARS_ORANGE)"╭─── "
+    if string match -e "$EXPERIMENTAL_FISH_LAUNCHED" "true" > /dev/null
+        set -l PREFIX_BEFORE_PWD "🐠🧪 "
+    end
+    set -l PREFIX $PREFIX_BEFORE_PWD(pwd)" "
+    if test (string length --visible $PREFIX) -gt $COLUMNS
+        set PREFIX $PREFIX_BEFORE_PWD(prompt_pwd)" "
+    end
+    _echo_padded \
+        $PREFIX \
+        (set_color $_FISH_LCARS_ORANGE)
+
+    set FISH_VCS_PROMPT (fish_vcs_prompt "%s")
+    if not string match -e "$FISH_VCS_PROMPT" "" > /dev/null
+        set -l PREFIX (set_color $_FISH_LCARS_ORANGE)"├─ "$FISH_VCS_PROMPT" "
+        echo $PREFIX
+    end
+    echo -n "├─ "
+    set suffix $suffix"
+" (set_color $_FISH_LCARS_ORANGE) "│"
 
     set _FISH_PROMPT_AFTER_FIRST_RUN true
 
-    echo -n -s (set_color $fish_color_user) "$USER" $normal @ (set_color $color_host) (prompt_hostname) $normal $_VCS $normal $suffix " "
+    echo -n -s (set_color $fish_color_user) "$USER" $normal @ (set_color $color_host) (prompt_hostname) $normal $suffix " "
 end

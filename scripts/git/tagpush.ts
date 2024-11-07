0eb0e226506a74d347ec8419421863e626205bda
Lucas Garron <code@garron.net>
2024-11-06 17:18:34 -0800

    Restore `tagpush`.

--- scripts/git/tagpush.ts
+++ scripts/git/tagpush.ts
@@ -8,43 +8,39 @@ const { binary, command, flag, run } = await import("cmd-ts");
 const SEPARATOR = "--------";
 
 const app = command({
-	name: "tagpush",
-	args: {
-		retag: flag({
-			description: "Remove an existing tag and retag if if it exists.",
-			long: "retag",
-		}),
-		completions: flag({
-			description: "Print completions",
-			long: "retag",
-		}),
-	},
-	handler: async ({ retag }) => {
-		const version = $`version`;
-		const previousCommitVersion = $`version --previous`;
+  name: "tagpush",
+  args: {
+    retag: flag({
+      description: "Remove an existing tag and retag if if it exists.",
+      long: "retag",
+    }),
+  },
+  handler: async ({ retag }) => {
+    const version = $`version`;
+    const previousCommitVersion = $`version --previous`;
 
-		if (version === previousCommitVersion) {
-			console.error(
-				"Project version did not change since last commit. Halting `tagpush`.",
-			);
-			exit(1);
-		}
+    if (version === previousCommitVersion) {
+      console.error(
+        "Project version did not change since last commit. Halting `tagpush`.",
+      );
+      exit(1);
+    }
 
-		if (retag) {
-			stdout.write("Tag was previously at at commit: ");
-			try {
-				console.log($`git rev-parse ${version}`);
-				console.log(SEPARATOR);
-				$`rmtag ${version}`;
-				console.log(SEPARATOR);
-			} catch {
-				console.log("No old tag.");
-			}
-		}
+    if (retag) {
+      stdout.write("Tag was previously at at commit: ");
+      try {
+        console.log($`git rev-parse ${version}`);
+        console.log(SEPARATOR);
+        $`rmtag ${version}`;
+        console.log(SEPARATOR);
+      } catch {
+        console.log("No old tag.");
+      }
+    }
 
-		$`git tag ${version}`;
-		$`git push origin ${version}`;
-	},
+    $`git tag ${version}`;
+    $`git push origin ${version}`;
+  },
 });
 
 await run(binary(app), process.argv);

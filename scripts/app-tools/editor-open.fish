35e3eb950f86ff8cab6ab74da6f67a78e1d67da2
Lucas Garron <code@garron.net>
2024-11-16 23:24:14 -0800

    [editor-open] Fix encoding received from AppleScript. 😕

--- scripts/app-tools/editor-open.fish
+++ scripts/app-tools/editor-open.fish
@@ -1,13 +1,15 @@
 #!/usr/bin/env -S fish --no-config
 
-set REALPATH (realpath $argv[1])
+set PATH_UTF8 (printf %s $argv[1] | iconv -f "UTF-8" -t "ISO-8859-1")
+echo $PATH_UTF8 >> ~/Downloads/debug.txt
+set REALPATH (realpath $PATH_UTF8)
 set PREFIX "$HOME/Library/Mobile Documents/iCloud~md~obsidian/Documents/"
 if string match "$PREFIX*" $REALPATH
   set OBSIDIAN_NOTE_PATH (string replace "$PREFIX" "" $REALPATH)
   set ENCODED_OBSIDIAN_NOTE_PATH (jq -rn --arg x $OBSIDIAN_NOTE_PATH "\$x|@uri")
   open -a "Obsidian" "obsidian://open?vault=Documents&file=$ENCODED_OBSIDIAN_NOTE_PATH"
 else
-  open -a "Visual Studio Code" $argv[1]
+  open -a "Visual Studio Code" $PATH_UTF8
 end
 
 return 1 # To avoid showing results in Quicksilver

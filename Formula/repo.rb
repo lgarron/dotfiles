b138809951f13f124366190040810ffdecbe11b6
Lucas Garron <code@garron.net>
2024-11-17 13:48:26 -0800

    [repo] Add formula for `repo`.

---
+++ Formula/repo.rb
@@ -0,0 +1,15 @@
+# frozen_string_literal: true
+
+class Repo < Formula
+  desc "⚙️ An opinionated tool for repo management."
+  homepage "https://github.com/lgarron/repo"
+  head "https://github.com/lgarron/repo.git", :branch => "main"
+
+  depends_on "rust" => :build
+
+  def install
+    system "cargo", "install", *std_cargo_args()
+
+    generate_completions_from_executable(bin/"repo", "--completions")
+  end
+end

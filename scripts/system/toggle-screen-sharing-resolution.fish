584930b0829f281cb8bcf8d27bff0cf97ccf2bae
Lucas Garron <code@garron.net>
2024-12-07 04:48:27 -0800

    [toggle-screen-sharing-resolution] Switch back to 1728×1080 for laptop screen.

--- scripts/system/toggle-screen-sharing-resolution.fish
+++ scripts/system/toggle-screen-sharing-resolution.fish
@@ -1,8 +1,7 @@
 #!/usr/bin/env -S fish --no-config
 
 if /Applications/RDM.app/Contents/MacOS/RDM -l | grep 2560 > /dev/null
-  /Applications/RDM.app/Contents/MacOS/RDM -w 1440 -h 900 -s 2
-  # /Applications/RDM.app/Contents/MacOS/RDM -w 1728 -h 1080 -s 2
+  /Applications/RDM.app/Contents/MacOS/RDM -w 1728 -h 1080 -s 2
 else
   /Applications/RDM.app/Contents/MacOS/RDM -w 2560 -h 1440 -s 2
 end

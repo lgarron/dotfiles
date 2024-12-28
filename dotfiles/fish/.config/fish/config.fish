50ff2a058fbd2e1a088220aa2640a0ff854d36f6
Lucas Garron <code@garron.net>
2024-12-28 07:01:33 -0800

    [config.fish] Add `tt` to use a file as pasteboard.
    Useful for `git` operations over SSH sessions (or in codespaces).

--- dotfiles/fish/.config/fish/config.fish
+++ dotfiles/fish/.config/fish/config.fish
@@ -70,12 +70,31 @@
 
     # abbr -a t "tee >(pbcopy)"
     function 📋
-      cat > /tmp/pbcopy.txt
-      cat /tmp/pbcopy.txt
-      cat /tmp/pbcopy.txt | pbcopy
-      rm /tmp/pbcopy.txt
+      # TODO: pipe?
+      set -l TEMP_FILE (mktemp -t "pbcopy")
+      cat > $TEMP_FILE
+      cat $TEMP_FILE
+      cat $TEMP_FILE | pbcopy
+      rm $TEMP_FILE
     end
     abbr -a t "📋"
+
+    function tt
+      if set SHELL_PASTEBOARD_FILE
+        rm -f $_FISH_SHELL_PASTEBOARD_FILE
+      end
+      set -gx _FISH_SHELL_PASTEBOARD_FILE (mktemp -t "pasteboard")
+      cat > $_FISH_SHELL_PASTEBOARD_FILE
+      cat $_FISH_SHELL_PASTEBOARD_FILE
+    end
+    function tt_paste
+      set -l cmd (commandline -op)
+      if [ (count $cmd) -lt 2 ]
+        return 1
+      end
+      cat $_FISH_SHELL_PASTEBOARD_FILE
+    end
+    abbr -a tt --position anywhere --function tt_paste
 
     function o
       if [ (count $argv) = 0 ]

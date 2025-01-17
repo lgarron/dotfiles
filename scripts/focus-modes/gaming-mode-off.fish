4c7377a43674a65e1c1e5df8f3ecaebe6fd4460c
Lucas Garron <code@garron.net>
2025-01-17 03:44:45 -0800

    Update yearly folders.

--- scripts/focus-modes/gaming-mode-off.fish
+++ scripts/focus-modes/gaming-mode-off.fish
@@ -2,7 +2,7 @@
 
 /Applications/Arq.app/Contents/Resources/arqc pauseBackups 0 > /dev/null
 
-defaults write com.apple.screencapture location "'/Users/lgarron/Dropbox (Maestral)/Screenshots/Germain Screenshots/Germain Screenshots 2024'"
+defaults write com.apple.screencapture location "'/Users/lgarron/Dropbox (Maestral)/Screenshots/Germain Screenshots/Germain Screenshots 2025'"
 killall SystemUIServer
 
 osascript -e 'quit app "Audio Hijack"'

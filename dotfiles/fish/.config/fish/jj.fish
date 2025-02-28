5166c4770e7dccddd981afe93f05dec6c592ea0e
Lucas Garron <code@garron.net>
2025-02-28 11:56:20 -0800

    [jj.fish] Refactor.

--- dotfiles/fish/.config/fish/jj.fish
+++ dotfiles/fish/.config/fish/jj.fish
@@ -2,37 +2,29 @@
 
     abbr -a j "jj"
 
+    function _fish_abbr_jj_subcommand
+        set -l abbrevation $argv[1]
+        set -l expansion $argv[2]
+        abbr -a "j$abbrevation" "jj $expansion"
+        abbr_subcommand jj $abbrevation $expansion
+    end
+
     # m⎵ → main
     abbr_exceptsubcommand_arg jj m main
 
-    abbr -a "jb" "jj bookmark"
-    abbr_subcommand jj b bookmark
-    abbr_subcommand jj bs "bookmark set"
+    _fish_abbr_jj_subcommand "a" "abandon"
+    _fish_abbr_jj_subcommand "d" "diff"
+    _fish_abbr_jj_subcommand "e" "edit"
+    _fish_abbr_jj_subcommand "gp" "git push"
+    _fish_abbr_jj_subcommand "l" "log"
+    _fish_abbr_jj_subcommand "m" "bookmark set main"
+    _fish_abbr_jj_subcommand "n" "new"
+    _fish_abbr_jj_subcommand "p" "git push"
+    _fish_abbr_jj_subcommand "s" "status"
+
+    _fish_abbr_jj_subcommand "b" "bookmark"
+    _fish_abbr_jj_subcommand "bs" "bookmark set"
     abbr_subcommand_arg jj s set bookmark
-    abbr -a "jbs" "jj bookmark set"
-    abbr -a "jm" "jj bookmark set main"
 
-    abbr -a "je" "jj edit"
-    abbr_subcommand jj e edit
-
-    abbr -a "js" "jj status"
-    abbr_subcommand jj s status
-
-    abbr -a "jn" "jj new"
-    abbr_subcommand jj n new
-
-    abbr -a "jd" "jj diff"
-
-    abbr -a "jde" "jj describe"
-    abbr_subcommand jj de describe
+    _fish_abbr_jj_subcommand "de" "describe"
     abbr -a jdd --set-cursor "jj describe --message \"%" # Special shortened abbreviation
-
-    abbr -a "jl" "jj log"
-    abbr_subcommand jj l log
-
-    abbr -a "jgp" "jj git push"
-    abbr_subcommand jj gp "git push"
-    abbr -a "jp" "jj git push"
-
-    abbr -a "ja" "jj abandon"
-    abbr_subcommand jj a abandon

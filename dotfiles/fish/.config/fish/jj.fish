3e472b35992d29563c0917b6746f0bdddf0689d8
Lucas Garron <code@garron.net>
2025-03-06 04:24:03 -0800

    [jj.fish] Add more abbreviations.

--- dotfiles/fish/.config/fish/jj.fish
+++ dotfiles/fish/.config/fish/jj.fish
@@ -14,20 +14,23 @@
 
     _fish_abbr_jj_subcommand "a" "abandon"
     _fish_abbr_jj_subcommand "e" "edit"
-    _fish_abbr_jj_subcommand "gp" "git push"
     _fish_abbr_jj_subcommand "l" "log"
     _fish_abbr_jj_subcommand "m" "bookmark set main"
     _fish_abbr_jj_subcommand "n" "new"
-    _fish_abbr_jj_subcommand "p" "git push"
     _fish_abbr_jj_subcommand "s" "status"
 
     _fish_abbr_jj_subcommand "b" "bookmark"
     _fish_abbr_jj_subcommand "bs" "bookmark set"
+    _fish_abbr_jj_subcommand "bl" "bookmark list"
     abbr_subcommand_arg jj s set bookmark
+    abbr_subcommand_arg jj l list bookmark
 
     _fish_abbr_jj_subcommand "de" "describe"
     abbr -a jdd --set-cursor "jj describe --message \"%" # Special shortened abbreviation
 
+    _fish_abbr_jj_subcommand "gf" "git fetch"
+    _fish_abbr_jj_subcommand "p" "git push"
+    _fish_abbr_jj_subcommand "gp" "git push"
     abbr -a "jpp" "jj bookmark set main && jj git push"
 
     _fish_abbr_jj_subcommand "d" "diff"

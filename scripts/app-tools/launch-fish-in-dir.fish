173304e2c140cd1ae732e31f41a6d693b1b704a2
Lucas Garron <code@garron.net>
2025-04-01 03:09:43 -0700

    [launch-fish-in-dir.fish] The unreliable encoding whack-a-mole continues.

--- scripts/app-tools/launch-fish-in-dir.fish
+++ scripts/app-tools/launch-fish-in-dir.fish
@@ -1,5 +1,5 @@
 #!/opt/homebrew/bin/fish --no-config
 
-set PATH_UTF8 (printf %s $argv[1] | iconv -f "UTF-8" -t "ISO-8859-1")
-set PATH_BASE64 (printf %s $PATH_UTF8 | base64)
+# set PATH_UTF8 (printf %s $argv[1] | iconv -f "UTF-8" -t "ISO-8859-1")
+set PATH_BASE64 (printf %s $argv[1] | base64)
 /opt/homebrew/bin/fish --interactive --init-command "cd-dir-from-iterm-base64 "(string escape $PATH_BASE64)

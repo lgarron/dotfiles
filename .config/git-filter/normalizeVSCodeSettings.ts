bdb6b33996953d12d1daf79f1ca95b81fe3c4483
Lucas Garron <code@garron.net>
2025-04-19 21:46:41 -0700

    `make format`

--- .config/git-filter/normalizeVSCodeSettings.ts
+++ .config/git-filter/normalizeVSCodeSettings.ts
@@ -1,7 +1,7 @@
 #!/usr/bin/env bun
 
 import { stdin } from "bun";
-import {type CommentObject, parse, stringify, } from "comment-json";
+import { type CommentObject, parse, stringify } from "comment-json";
 
 const WINDOW_ZOOM_LEVEL = "window.zoomLevel";
 const WINDOW_ZOOM_LEVEL_NORMALIZED = 2;
@@ -11,12 +11,12 @@ const EDITOR_FORMAT_ON_SAVE_NORMALIZED = true;
 
 const parsed: CommentObject = parse(await stdin.text()) as CommentObject;
 
-if(EDITOR_FORMAT_ON_SAVE in parsed) {
-  parsed[EDITOR_FORMAT_ON_SAVE] = EDITOR_FORMAT_ON_SAVE_NORMALIZED
+if (EDITOR_FORMAT_ON_SAVE in parsed) {
+  parsed[EDITOR_FORMAT_ON_SAVE] = EDITOR_FORMAT_ON_SAVE_NORMALIZED;
 }
 
-if(WINDOW_ZOOM_LEVEL in parsed) {
-  parsed[WINDOW_ZOOM_LEVEL] = WINDOW_ZOOM_LEVEL_NORMALIZED
+if (WINDOW_ZOOM_LEVEL in parsed) {
+  parsed[WINDOW_ZOOM_LEVEL] = WINDOW_ZOOM_LEVEL_NORMALIZED;
 }
 
 console.log(stringify(parsed, null, "\t"));

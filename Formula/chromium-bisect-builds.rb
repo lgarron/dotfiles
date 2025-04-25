13e9a637fa3e907572a4ed9464c3e640397a845f
Lucas Garron <code@garron.net>
2025-04-25 11:28:48 -0700

    [chromium-bisect-builds] Adapt to use `uv` as the runner.

--- Formula/chromium-bisect-builds.rb
+++ Formula/chromium-bisect-builds.rb
@@ -4,15 +4,18 @@ class ChromiumBisectBuilds < Formula
   url "https://chromium.googlesource.com/chromium/src/+/HEAD/tools/bisect-builds.py?format=TEXT", using: :nounzip
   version "HEAD"
 
-  depends_on "python@3.10"
-
-  def python3
-    "python3.10"
-  end
+  depends_on "uv"
 
   def install
     system "base64", "-d", "-i", "bisect-builds.py", "-o", "chromium-bisect-builds"
+
+    # We can't easily use patch on base64, so we modify the file directly.
+    contents = IO.read "chromium-bisect-builds"
+    # We need Python 3.10 for `distutils`.
+    IO.write "chromium-bisect-builds", "#!/usr/bin/env uv run --script\n# /// script\n# requires-python = \"==3.10\"\n# ///\n\n" + contents
+
     chmod 0755, "chromium-bisect-builds"
     bin.install "chromium-bisect-builds"
   end
 end
+

6364f1aeff2dd88d71b53c3d6ff5c899824d347f
Lucas Garron <code@garron.net>
2025-04-28 09:59:32 -0700

    [VS Code] Normalize font setting in the git filter.

--- .config/git-filter/normalizeVSCodeSettings.ts
+++ .config/git-filter/normalizeVSCodeSettings.ts
@@ -9,6 +9,9 @@ const WINDOW_ZOOM_LEVEL_NORMALIZED = 2;
 const EDITOR_FORMAT_ON_SAVE = "editor.formatOnSave";
 const EDITOR_FORMAT_ON_SAVE_NORMALIZED = true;
 
+const EDITOR_FONT_FAMILY = "editor.fontFamily";
+const EDITOR_FONT_FAMILY_NORMALIZED = '"Comic Mono", Menlo, Monaco, monospace';
+
 const parsed: CommentObject = parse(await stdin.text()) as CommentObject;
 
 if (EDITOR_FORMAT_ON_SAVE in parsed) {
@@ -17,6 +20,10 @@ if (EDITOR_FORMAT_ON_SAVE in parsed) {
 
 if (WINDOW_ZOOM_LEVEL in parsed) {
   parsed[WINDOW_ZOOM_LEVEL] = WINDOW_ZOOM_LEVEL_NORMALIZED;
+}
+
+if (EDITOR_FONT_FAMILY in parsed) {
+  parsed[EDITOR_FONT_FAMILY] = EDITOR_FONT_FAMILY_NORMALIZED;
 }
 
 console.log(stringify(parsed, null, "\t"));

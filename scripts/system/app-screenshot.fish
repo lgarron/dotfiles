9ea7de7ea668a57356d7715e9d4c742c1159db3c
Lucas Garron <code@garron.net>
2025-05-13 19:16:53 -0700

    [app-screenshot] Use an absolute path to invoke `reveal-macos`.

--- scripts/system/app-screenshot.fish
+++ scripts/system/app-screenshot.fish
@@ -3,19 +3,22 @@
 # Full `fish` path is hardcoded so it works outside the shell (e.g. using Karabiner). Note that a symlink at `$HOME/Dropbox` doesn't seem to work for this.
 set PARENT_FOLDER "$HOME/Dropbox (Maestral)/Screenshots/Germain Screenshots/Germain App Screenshots/"
 
+# We use an absolute path because we're invoked by `Karabiner-Elements` without the normal Homebrew path.
+set REVEAL_MACOS $HOME/Code/git/github.com/lgarron/dotfiles/scripts/system/reveal-macos.swift
+
 set DATE_STRING (date "+%Y-%m-%d — %H-%M-%S")
 
 set WINDOW_ID_ARG ""
-if [ "$argv[1]" = "--front-window-shadowed-png" ]
-  set WINDOW_ID (osascript -e 'tell application (path to frontmost application as text) to id of window 1')
-  set EXTENSION "heic"
-  set WINDOW_ID_ARG -l$WINDOW_ID
-else if [ "$argv[1]" = "--front-window-shadowless-jpg" ]
-  set WINDOW_ID (osascript -e 'tell application (path to frontmost application as text) to id of window 1')
-  set EXTENSION "heic"
-  set WINDOW_ID_ARG -l$WINDOW_ID
+if [ "$argv[1]" = --front-window-shadowed-png ]
+    set WINDOW_ID (osascript -e 'tell application (path to frontmost application as text) to id of window 1')
+    set EXTENSION heic
+    set WINDOW_ID_ARG -l$WINDOW_ID
+else if [ "$argv[1]" = --front-window-shadowless-jpg ]
+    set WINDOW_ID (osascript -e 'tell application (path to frontmost application as text) to id of window 1')
+    set EXTENSION heic
+    set WINDOW_ID_ARG -l$WINDOW_ID
 else
-  set EXTENSION "heic"
+    set EXTENSION heic
 end
 
 set TEMP_FILE $PARENT_FOLDER/$DATE_STRING" — temp "(time random 1 10000000000).$EXTENSION
@@ -36,7 +39,7 @@ echo "taken!"
 # - Minecraft
 #   - `short name`: missing value
 #   - `name`: "java"
-#   - `bundle identifier`: vissing value
+#   - `bundle identifier`: missing value
 #
 # So we use `short name` if available, else `name`.
 set APP_NAME (osascript -e 'tell application "System Events"
@@ -46,8 +49,8 @@ set APP_NAME (osascript -e 'tell application "System Events"
 	end if
 	appName
 end tell')
-if string match -e $APP_NAME java
-  set APP_NAME "Minecraft"
+if string match --quiet -entire $APP_NAME java
+    set APP_NAME Minecraft
 end
 echo $APP_NAME
 set APP_FOLDER "$PARENT_FOLDER/$APP_NAME"
@@ -57,11 +60,11 @@ set FILE_NAME "$APP_FOLDER/$DATE_STRING — $APP_NAME.$EXTENSION"
 mv $TEMP_FILE $FILE_NAME
 
 # TODO: better arg handling
-if [ "$argv[1]" = "--reveal" ]
-  reveal-macos $FILE_NAME
+if [ "$argv[1]" = --reveal ]
+    $REVEAL_MACOS $FILE_NAME
 end
-if [ "$argv[2]" = "--reveal" ]
-  reveal-macos $FILE_NAME
+if [ "$argv[2]" = --reveal ]
+    $REVEAL_MACOS $FILE_NAME
 end
 # /Applications/ImageOptim.app/Contents/MacOS/ImageOptim $FILE_NAME &
 

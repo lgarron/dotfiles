0d2b428e84c7ddc8e1b2fa73bcb499ab6936e0f4
Lucas Garron <code@garron.net>
2025-05-14 03:56:34 -0700

    [jclone.rb] Fix class name.

--- Formula/jclone.rb
+++ Formula/jclone.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 
-class Gclone < Formula
+class Jclone < Formula
   desc "jj clone script."
   homepage "https://github.com/lgarron/dotfiles"
   head "https://github.com/lgarron/dotfiles.git", :branch => "main"

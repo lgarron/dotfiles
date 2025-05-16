f884b1a08846cfca6a74bc1b38d9d05e2ba6db49
Lucas Garron <code@garron.net>
2025-05-16 02:06:24 -0700

    [openscad-lsp-topiary] Bump version.
    Hopefully picks up a fix for https://github.com/Leathong/openscad-LSP/issues/48#issuecomment-2886180367

--- Formula/openscad-lsp-topiary.rb
+++ Formula/openscad-lsp-topiary.rb
@@ -3,8 +3,8 @@
 class OpenscadLspTopiary < Formula
   desc "jj clone script."
   homepage "https://github.com/lgarron/dotfiles"
-  url "https://github.com/Leathong/openscad-LSP/releases/download/v1.2.7/topiary-mac.zip"
-  sha256 "4d1e9b0dc06516dc642a1e8d5b72e2080aec1a3ebee0bfd49b9674bb98f0a4a1"
+  url "https://github.com/Leathong/openscad-LSP/releases/download/v1.3.0/openscad-lsp-mac.zip"
+  sha256 "d058a5f93e2b7176289e0b3225028a6e26074f8c6179378e1da41a76bc5e8403"
 
   def install
     bin.install "topiary" => "openscad-lsp-topiary"

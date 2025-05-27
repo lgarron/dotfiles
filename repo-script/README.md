384685429cf40c0310830c02f436cb33b74067f0
Lucas Garron <code@garron.net>
2025-05-27 11:21:49 -0700

    [VS Code] Add filaments to `scad` snippets.

---
+++ repo-script/README.md
@@ -0,0 +1,1 @@
+This folder would be called `script` in a normal repository, but it is named `repo-script` to avoid confusion with `scripts` (which is for scripts that are not specific to this repo).
---
+++ repo-script/vscode-snippets-update-scad-filaments.ts
@@ -0,0 +1,55 @@
+import { homedir } from "node:os";
+import { join } from "node:path";
+import { file } from "bun";
+import {
+  applyEdits,
+  findNodeAtLocation,
+  format,
+  modify,
+  parseTree,
+} from "jsonc-parser";
+
+const source = await file(
+  join(homedir(), "Code/git/github.com/lgarron/scad/filament_color.scad"),
+).text();
+
+const jsoncFile = file(
+  join(homedir(), "Library/Application Support/Code/User/snippets/scad.json"),
+);
+let jsonc = await jsoncFile.text();
+
+const existingDOM = parseTree(jsonc);
+
+function insertFilament(filamentName: string) {
+  // `jconc-parser`… does not have a very ergonomic API. So we have to do this check ourselves.
+  const alreadyPresent = Boolean(
+    findNodeAtLocation(existingDOM, [filamentName]),
+  );
+  if (alreadyPresent) {
+    return;
+  }
+  jsonc = applyEdits(
+    jsonc,
+    modify(
+      jsonc,
+      [filamentName],
+      {
+        prefix: filamentName,
+        body: [filamentName],
+      },
+      {},
+    ),
+  );
+}
+
+for (const line of source.split("\n")) {
+  const match = line.match(/^(\S+) =/);
+  if (match) {
+    insertFilament(match[1]);
+  }
+}
+
+jsonc = applyEdits(jsonc, format(jsonc, undefined, {}));
+console.log(jsonc);
+
+jsoncFile.write(jsonc);

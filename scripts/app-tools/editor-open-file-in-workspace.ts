d0c9d396f0c75a1808e1bd4449ba8ea3fa4c6f89
Lucas Garron <code@garron.net>
2025-05-28 12:25:05 -0700

    [editor-open-file-in-workspace] Fix incorrect directory calculation.

--- scripts/app-tools/editor-open-file-in-workspace.ts
+++ scripts/app-tools/editor-open-file-in-workspace.ts
@@ -1,10 +1,19 @@
 #!/usr/bin/env bun
 
+import { lstat } from "node:fs/promises";
+import { dirname } from "node:path";
 import { exit } from "node:process";
 import { $, argv } from "bun";
 
 const path = argv[2];
-const workspaceRootDir = await $`repo workspace root`.text();
+const dir = (await lstat(path)).isDirectory ? dirname(path) : path;
+const workspaceRootDir = await (async () => {
+  try {
+    await $`cd ${dir} && repo workspace root`.text();
+  } catch {
+    return dir;
+  }
+})();
 
 await $`code ${workspaceRootDir} && code --reuse-window ${path}`;
 

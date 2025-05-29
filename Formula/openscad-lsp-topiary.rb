b0190e7b25f51172333eac38735599a88854867b
Lucas Garron <code@garron.net>
2025-05-29 09:16:45 -0700

    [openscad-lsp-topiary] Update description and homepage.

--- Formula/openscad-lsp-topiary.rb
+++ Formula/openscad-lsp-topiary.rb
@@ -1,8 +1,8 @@
 # frozen_string_literal: true
 
 class OpenscadLspTopiary < Formula
-  desc "jj clone script."
-  homepage "https://github.com/lgarron/dotfiles"
+  desc "OpenSCAD LSP binary"
+  homepage "https://github.com/Leathong/openscad-LSP"
   url "https://github.com/Leathong/openscad-LSP/releases/download/v2.0.1/openscad-lsp-aarch64-apple-darwin.tar.xz"
   sha256 "3d9a2656097c6775ae60094fcd84162032c86bdfda719baae3fa2efeb33f223f"
 

2ffd0c2fc058ca99155abcdda4e995192c3504b1
Lucas Garron <code@garron.net>
2025-06-01 00:53:48 -0700

    [gg] Add `gg` cask.

---
+++ Casks/gg.rb
@@ -0,0 +1,10 @@
+cask "gg" do
+  version "0.27.0"
+  sha256 "56a9973d612c513548bb20ad7ecfb9f7991199139823b0c661f944f55d5873d4"
+
+  url "https://github.com/gulbanana/gg/releases/download/v0.27.0/gg_0.27.0_universal.dmg"
+  name "gg"
+  homepage "https://github.com/gulbanana/gg"
+
+  app "gg.app"
+end

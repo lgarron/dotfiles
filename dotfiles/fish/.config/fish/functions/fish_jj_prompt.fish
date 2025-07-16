2f352d860a7930064428fb4d3033685e77854c12
Lucas Garron <code@garron.net>
2025-07-15 21:21:05 -0700

    [fish_jj_prompt.fish] Add timeouts.

--- dotfiles/fish/.config/fish/functions/fish_jj_prompt.fish
+++ dotfiles/fish/.config/fish/functions/fish_jj_prompt.fish
@@ -1,10 +1,13 @@
 function __fish_jj_num_commits
-    command jj log 2>/dev/null \
+    # timeout doesn't take `--` (at least on macOS), so we hope `$argv[1]` doesn't allow for an injection. 🤷
+    set DOT_PER_COMMIT (timeout --preserve-status 1s jj log 2>/dev/null \
         --no-graph \
         --ignore-working-copy \
         --color=always \
         --revisions $argv[1] \
-        --template "'.'" \
+        --template "'.'") || return 1
+
+    printf "%s" $DOT_PER_COMMIT \
         | wc -c \
         | tr -d " "
 end
@@ -12,13 +15,20 @@ end
 # From https://github.com/fish-shell/fish-shell/issues/11183#issuecomment-2699601115
 # Based on fish_jj_prompt and https://gist.github.com/hroi/d0dc0e95221af858ee129fd66251897e
 function fish_jj_prompt
+    # This doesn't work if we set it locally outside the function.
+    set -l TIMED_OUT "(timed out)"
+
     # If jj isn't installed, there's nothing we can do
     # Return 1 so the calling prompt can deal with it
     if not command -sq jj
         return 1
     end
+    # If `jj` can't find a root, we're probably not in a `jj` repo.
+    if not jj root &>/dev/null
+        return 1
+    end
     set -l info (
-        command jj log 2>/dev/null --no-graph --ignore-working-copy --color=always --revisions @ \
+        timeout --preserve-status 1s jj log 2>/dev/null --no-graph --ignore-working-copy --color=always --revisions @ \
             --template '
                     separate(
                         " ",
@@ -29,28 +39,27 @@ function fish_jj_prompt
                         if(hidden, label("hidden prefix", "(hidden)")),
                         if(immutable, label("node immutable", "◆")),
                     )
-            ' \
-            | string collect
+            ' || printf "%s" $TIMED_OUT
     )
     if test $pipestatus[1] -ne 0
         return 1
     end
     # TODO: Find a semantically safe way to avoid having to run this separately.
     set -l prompt_anchor_pretty (
-        command jj prompt-anchor-pretty --color=always
+        timeout --preserve-status 1s jj prompt-anchor-pretty --color=always || printf "%s" $TIMED_OUT
     )
-    set -l prompt_pushable_stack (__fish_jj_num_commits "prompt_pushable_stack")
-    set -l prompt_draft (__fish_jj_num_commits "prompt_draft")
-    set -l prompt_blank_fringe (__fish_jj_num_commits "prompt_blank_fringe")
+    set -l prompt_pushable_stack (__fish_jj_num_commits "prompt_pushable_stack" || printf "%s" $TIMED_OUT)
+    set -l prompt_draft (__fish_jj_num_commits "prompt_draft" || printf "%s" $TIMED_OUT)
+    set -l prompt_blank_fringe (__fish_jj_num_commits "prompt_blank_fringe" || printf "%s" $TIMED_OUT)
 
     # TODO: if the anchor is not a bookmark, show the distance to `trunk()` / `main`.
     set anchor_suffix "$prompt_anchor_pretty" "+$prompt_pushable_stack pushable"
-    if [ "$prompt_draft" -gt 0 ]
+    if string match --quiet --entire -- "$prompt_draft" "$TIMED_OUT" || [ "$prompt_draft" -gt 0 ]
         set anchor_suffix $anchor_suffix (set_color yellow)"+$prompt_draft unpushable"(set_color normal)
     end
-    if [ "$prompt_blank_fringe" -gt 0 ]
+    if string match --quiet --entire -- "$prompt_blank_fringe" "$TIMED_OUT" || [ "$prompt_blank_fringe" -gt 0 ]
         set anchor_suffix $anchor_suffix (set_color brgreen)"+$prompt_blank_fringe empty"(set_color normal)
     end
 
-    printf "%s (%s)" $info (string join " " -- $anchor_suffix)
+    printf "%s (%s)" "$info" (string join " " -- $anchor_suffix)
 end

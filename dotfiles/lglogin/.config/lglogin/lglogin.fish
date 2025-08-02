23e406ae2c92991430ffdbf42ea8239d7933c9c4
Lucas Garron <code@garron.net>
2025-08-01 19:41:01 -0700

    [lglogin.fish] Comment out `OptGuideOnDeviceModel` for now.

--- dotfiles/lglogin/.config/lglogin/lglogin.fish
+++ dotfiles/lglogin/.config/lglogin/lglogin.fish
@@ -88,7 +88,8 @@
     rm_and_log $HOME/.pub-cache
 
     # See: https://github.com/lgarron/first-world/issues/239
-    rm_and_log "$HOME/Library/Application Support/Google/Chrome/OptGuideOnDeviceModel/"
+    # This is currently commented because I'm teting out using a locked folder instead.
+    # rm_and_log "$HOME/Library/Application Support/Google/Chrome/OptGuideOnDeviceModel/"
 
 # niceplz
 

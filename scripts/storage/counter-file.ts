cc05a1ff6bc0318ee793b78a196e8d97d1f77b97
Lucas Garron <code@garron.net>
2025-08-01 19:43:35 -0700

    [counter-file] Use an ergonomic timestamp.

--- scripts/storage/counter-file.ts
+++ scripts/storage/counter-file.ts
@@ -1,5 +1,6 @@
 import { exit } from "node:process";
 import { argv, file, write } from "bun";
+import { ErgonomicDate } from "ergonomic-date";
 
 if (argv.length < 2) {
   exit(1);
@@ -9,5 +10,5 @@ const [fileName, jsonKey] = argv.slice(2);
 
 const f = file(fileName);
 const contents = (await f.exists()) ? await file(fileName).json() : {};
-(contents[jsonKey] ??= []).push(new Date().toISOString());
+(contents[jsonKey] ??= []).push(new ErgonomicDate().multipurposeTimestamp);
 await write(f, JSON.stringify(contents, null, "  "));

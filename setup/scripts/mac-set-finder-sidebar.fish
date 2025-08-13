21e2d1186b5536fb184242f29861566a66e0b709
Lucas Garron <code@garron.net>
2025-08-13 09:49:16 -0700

    Handle URL encoding in a function.

--- setup/scripts/mac-set-finder-sidebar.fish
+++ setup/scripts/mac-set-finder-sidebar.fish
@@ -3,41 +3,45 @@
 # TODO: figure out how to make this more declarative while taking `sudo` permission into account.
 command -v mysides || brew install --cask mysides
 
+function to_file_url
+    printf "file://%s" (string escape --style url $argv[1])
+end
+
 set -l _CURRENT_HOSTNAME (hostname -s)
-if contains $_CURRENT_HOSTNAME "Germain"
+if contains $_CURRENT_HOSTNAME Germain
 
-  # TODO: this permission prompt requires user interaction, and doesn't work over e.g. SSH?
-  sfltool clear com.apple.LSSharedFileList.FavoriteItems
+    # TODO: this permission prompt requires user interaction, and doesn't work over e.g. SSH?
+    sfltool clear com.apple.LSSharedFileList.FavoriteItems
 
-  mysides add "lgarron" "file://$HOME"
-  mysides add "Downloads" "file://$HOME/Downloads/"
-  mysides add "Uploads" "file://$HOME/Uploads/"
-  mysides add "iCloud Drive" "file://$HOME/Library/Mobile%20Documents/com~apple~CloudDocs"
-  mysides add "Dropbox" "file://$HOME/Dropbox%20(Maestral)/"
-  mysides add "3D printing" "file://$HOME/Dropbox%20(Maestral)/3D%20Printing/"
-  mysides add "Volumes" "file:///Volumes/"
-  mysides add "Trenzalore Locations" "file:///Volumes/Trenzalore/Meta/Trenzalore%20Locations/"
-  mysides add "2025" "file:///Volumes/Trenzalore/Video%20Editing/2025/"
+    mysides add lgarron (to_file_url "$HOME")
+    mysides add Downloads (to_file_url "$HOME/Downloads/")
+    mysides add Uploads (to_file_url "$HOME/Uploads/")
+    mysides add "iCloud Drive" (to_file_url "$HOME/Library/Mobile Documents/com~apple~CloudDocs")
+    mysides add Dropbox (to_file_url "$HOME/Dropbox (Maestral)/")
+    mysides add "3D printing" (to_file_url "$HOME/Dropbox (Maestral)/3D Printing/")
+    mysides add Volumes (to_file_url "/Volumes/")
+    mysides add "Trenzalore Locations" (to_file_url "/Volumes/Trenzalore/Meta/Trenzalore Locations/")
+    mysides add 2025 (to_file_url "/Volumes/Trenzalore/Video Editing/2025/")
 
-else if contains $_CURRENT_HOSTNAME "Pythagoras"
+else if contains $_CURRENT_HOSTNAME Pythagoras
 
-  # TODO: this permission prompt requires user interaction, and doesn't work over e.g. SSH?
-  sfltool clear com.apple.LSSharedFileList.FavoriteItems
+    # TODO: this permission prompt requires user interaction, and doesn't work over e.g. SSH?
+    sfltool clear com.apple.LSSharedFileList.FavoriteItems
 
-  mysides add "lgarron" "file:///Users/lgarron/"
-  mysides add "Dropbox" "file:///Volumes/Samos/.CloudStorage/Data/Dropbox/"
-  mysides add "Google Drive" "file:///Users/lgarron/Google%20Drive/"
-  mysides add "OneDrive-Personal" "file:///Users/lgarron/Library/CloudStorage/OneDrive-Personal/"
-  mysides add "Downloads" "file:///Users/lgarron/Downloads/"
-  mysides add "Desktop" "file:///Users/lgarron/Desktop/"
-  mysides add "Volumes" "file:///Volumes/"
-  mysides add "2025" "file:///Volumes/Trenzalore/Video%20Editing/2025/"
-  mysides add "Trenzalore Locations" "file:///Volumes/Trenzalore/Meta/Trenzalore%20Locations/"
-  mysides add "HEVC archived.savedSearch" "file:///Users/lgarron/Library/Saved%20Searches/HEVC%20archived.savedSearch"
-  mysides add "HEVC archived (from testing).savedSearch" "file:///Users/lgarron/Library/Saved%20Searches/HEVC%20archived%20(from%20testing).savedSearch"
+    mysides add lgarron (to_file_url "/Users/lgarron/")
+    mysides add Dropbox (to_file_url "/Volumes/Samos/.CloudStorage/Data/Dropbox/")
+    mysides add "Google Drive" (to_file_url "/Users/lgarron/Google Drive/")
+    mysides add OneDrive-Personal (to_file_url "/Users/lgarron/Library/CloudStorage/OneDrive-Personal/")
+    mysides add Downloads (to_file_url "/Users/lgarron/Downloads/")
+    mysides add Desktop (to_file_url "/Users/lgarron/Desktop/")
+    mysides add Volumes (to_file_url "/Volumes/")
+    mysides add 2025 (to_file_url "/Volumes/Trenzalore/Video Editing/2025/")
+    mysides add "Trenzalore Locations" (to_file_url "/Volumes/Trenzalore/Meta/Trenzalore Locations/")
+    mysides add "HEVC archived.savedSearch" (to_file_url "/Users/lgarron/Library/Saved Searches/HEVC archived.savedSearch")
+    mysides add "HEVC archived (from testing).savedSearch" (to_file_url "/Users/lgarron/Library/Saved Searches/HEVC archived (from testing).savedSearch")
 
 else
 
-  echo "Unrecognized machine. Leaving the Finder sidebar alone."
+    echo "Unrecognized machine. Leaving the Finder sidebar alone."
 
 end

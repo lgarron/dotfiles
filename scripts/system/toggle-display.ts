9bebd25a98f558ab91e281fd185f1df6a7869867
Lucas Garron <code@garron.net>
2025-09-04 21:21:13 -0700

    [toggle-display] Add `--list-displays` flag.

--- scripts/system/toggle-display.ts
+++ scripts/system/toggle-display.ts
@@ -1,5 +1,6 @@
 #!/usr/bin/env -S bun run --
 
+import { default as assert } from "node:assert";
 import { $ } from "bun";
 
 import {
@@ -8,17 +9,33 @@ import {
   command,
   positional,
   run,
+  optional,
+  flag,
 } from "cmd-ts-too";
 
 const app = command({
   name: "toggle-display",
   args: {
     displayName: positional({
-      type: cmdString,
+      // TODO: this is only optional if `--listDisplays` is not specified.
+      type: optional( cmdString),
+      // Note: the fact that the field name and its value are similar is a coincidence. (!)
       displayName: "Display name",
     }),
+    listDisplays: flag({
+      description: "List displays (instead of toggling any).",
+      long: "list-displays"
+    }),
   },
-  handler: async ({ displayName }) => {
+  handler: async ({ displayName , listDisplays}) => {
+    if (listDisplays) {
+      const jsonSequence = await $`betterdisplaycli get -identifiers`.text();
+      const json: {deviceType: "Display" | "VirtualScreen" | "DisplayGroup",name: string}[] = JSON.parse(`[${jsonSequence}]`);
+      const names = json.filter(data => data.deviceType !== "DisplayGroup").map(data => data.name);
+      console.log(names.join("\n"));
+      return;
+    }
+    assert(displayName);
     const on =
       (
         await $`betterdisplaycli get --name="${displayName}" --connected`.text()

30851859f5d682cefe5f46a660a0742183313f09
Lucas Garron <code@garron.net>
2025-09-12 00:04:12 -0700

    Trampoline `launchd` to pick up path and env vars.

---
+++ setup/scripts/trampoline.fish
@@ -0,0 +1,6 @@
+#!/opt/homebrew/bin/fish --no-config
+
+source /Users/lgarron/Code/git/github.com/lgarron/dotfiles/dotfiles/fish/.config/fish/path.fish
+source /Users/lgarron/Code/git/github.com/lgarron/dotfiles/dotfiles/xdg-basedir-workarounds/.config/fish/xdg-basedir-workarounds.fish
+
+$argv

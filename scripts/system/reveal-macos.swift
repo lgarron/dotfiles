841fa56ea5e4c92d9f4e53aca55c87fcf05ff703
Lucas Garron <code@garron.net>
2025-10-08 12:05:01 -0700

    [reveal-macos] Bring Finder to the foreground if needed.
    See: https://github.com/lgarron/first-world/issues/256

--- scripts/system/reveal-macos.swift
+++ scripts/system/reveal-macos.swift
@@ -17,5 +17,15 @@ let theURLs = theFilePaths.map(toURL)
 
 NSWorkspace.shared.activateFileViewerSelecting(theURLs)
 
+// Workaround for https://github.com/lgarron/first-world/issues/256
+if let finder = NSWorkspace.shared.runningApplications.first(where: { $0.bundleIdentifier == "com.apple.finder" }) {
+    if (!finder.isActive) {
+        fputs("Finder did not automatically come to the foreground, possibly due to a macOS bug. Invoking a workaround.\n", stderr)
+        finder.activate()
+    }
+} else {
+    fputs("Finder does not appear to be running‽\n", stderr)
+}
+
 // Print the paths so we can pass through a file to `stdin` of another command.
 print(theFilePaths.joined(separator: "\n"), terminator: "")

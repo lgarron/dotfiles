accd6ead65f7553cb993131c64d1956ef586608b
Lucas Garron <code@garron.net>
2025-10-13 12:03:31 -0700

    [thirdparty-oavif] Add a formula for `oavif`.

---
+++ Formula/thirdparty-oavif.rb
@@ -0,0 +1,17 @@
+# frozen_string_literal: true
+
+class ThirdpartyOavif < Formula
+  desc "🖥️ Tool for target quality AVIF encoding using fssimu2, a fast perceptual image quality metric."
+  homepage "https://github.com/gianni-rosato/oavif"
+  url "https://github.com/gianni-rosato/oavif/archive/refs/tags/0.1.2.zip"
+  sha256 "a36e3b4000e761bec1301f17391790ba38e6b691dfeaf7d1e2e9c4cab442b9a7"
+  head "https://github.com/gianni-rosato/oavif.git", :branch => "main"
+
+  depends_on "zig" => :build
+  # TODO: do we need to declare other deps?
+  # https://github.com/gianni-rosato/oavif?tab=readme-ov-file#compilation
+
+  def install
+    system "zig", "build", "--release=fast", *std_zig_args
+  end
+end

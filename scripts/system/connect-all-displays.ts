2e24497f203079e45ade7a063f6b8a581423216a
Lucas Garron <code@garron.net>
2025-11-05 15:24:04 -0800

    [connect-all-displays] Use `betterdisplaycli.js`.

--- scripts/system/connect-all-displays.ts
+++ scripts/system/connect-all-displays.ts
@@ -1,16 +1,13 @@
 #!/usr/bin/env -S bun run --
 
+import { connectAllDisplays } from "betterdisplaycli";
 import { binary, command, run } from "cmd-ts-too";
-import { PrintableShellCommand } from "printable-shell-command";
 
 const app = command({
-  name: "toggle-display",
+  name: "connect-all-displays",
   args: {},
   handler: async () => {
-    new PrintableShellCommand("betterdisplaycli", [
-      "perform",
-      "--connectAllDisplays",
-    ]);
+    await connectAllDisplays();
   },
 });
 

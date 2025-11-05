a35cfc47c1819daf4c5a7d2576cd59682faab385
Lucas Garron <code@garron.net>
2025-11-05 12:46:22 -0800

    [connect-all-displays] Fix `betterdisplaycli` invocation.
    I'm not sure why the incorrect command… sometimes worked?
    https://github.com/waydabber/BetterDisplay/discussions/4798#discussioncomment-14883856

--- scripts/system/connect-all-displays.ts
+++ scripts/system/connect-all-displays.ts
@@ -1,19 +1,15 @@
 #!/usr/bin/env -S bun run --
 
-import { $ } from "bun";
 import { binary, command, run } from "cmd-ts-too";
+import { PrintableShellCommand } from "printable-shell-command";
 
 const app = command({
   name: "toggle-display",
   args: {},
   handler: async () => {
-    // This is a workaround `betterdisplaycli` hanging after successfully executing this command (at least on one computer).
-    await Promise.all([
-      $`betterdisplaycli connectAllDisplays`,
-      (async () => {
-        await new Promise((resolve) => setTimeout(resolve, 5000));
-        throw new Error("Timeout");
-      })(),
+    new PrintableShellCommand("betterdisplaycli", [
+      "perform",
+      "--connectAllDisplays",
     ]);
   },
 });

b6ff3627cdde735b0433b12b464eeb06c6d64d02
Lucas Garron <code@garron.net>
2025-11-05 13:31:32 -0800

    [connect-all-displays] Add a notification upon success.

--- scripts/system/connect-all-displays.ts
+++ scripts/system/connect-all-displays.ts
@@ -1,13 +1,30 @@
 #!/usr/bin/env -S bun run --
 
-import { connectAllDisplays } from "betterdisplaycli";
+import { connectAllDisplays, getAllDevices } from "betterdisplaycli";
 import { binary, command, run } from "cmd-ts-too";
+import { PrintableShellCommand } from "printable-shell-command";
 
 const app = command({
   name: "connect-all-displays",
   args: {},
   handler: async () => {
     await connectAllDisplays();
+
+    try {
+      const numDisplays = (await getAllDevices({ ignoreDisplayGroups: true }))
+        .length;
+      await new PrintableShellCommand("terminal-notifier", [
+        ["-title", "Connect all displays"],
+        [
+          "-message",
+          `${numDisplays} display${numDisplays === 1 ? "" : "s"} are now connected.`,
+        ],
+      ]).shellOut();
+    } catch (e) {
+      console.error(
+        `Error trying to invoke \`terminal-notifier\`. Ignoring: ${e}`,
+      );
+    }
   },
 });
 

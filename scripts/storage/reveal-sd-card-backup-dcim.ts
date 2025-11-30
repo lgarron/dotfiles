49197903c59fcb7030ef43003adbbc549c783a58
Lucas Garron <code@garron.net>
2025-11-30 15:08:12 -0800

    [reveal-sd-card-backup-dcim.ts] Switch to `cmd-ts-too` for commandline parsing.

--- scripts/storage/reveal-sd-card-backup-dcim.ts
+++ scripts/storage/reveal-sd-card-backup-dcim.ts
@@ -6,169 +6,186 @@ import { stat } from "node:fs/promises";
 import { homedir } from "node:os";
 import { extname, join } from "node:path";
 import { argv, exit } from "node:process";
+import { binary, command, positional, run } from "cmd-ts-too";
+import { ExistingPath } from "cmd-ts-too/batteries/fs";
 import { PrintableShellCommand } from "printable-shell-command";
 
-const filePath = argv[2];
-if (!filePath) {
-  throw new Error("Must supply a path.");
-}
+const app = command({
+  name: "reveal-sd-card-backup-dcim",
+  args: {
+    filePath: positional({
+      type: ExistingPath,
+      displayName: "File",
+    }),
+  },
+  handler: async ({ filePath }) => {
+    const destinationRoot: string = (
+      await Bun.file(
+        join(homedir(), "/.config/sd-card-backup/config.json"),
+      ).json()
+    ).destination_root;
+    const destinationRootParts = destinationRoot.split("/");
+    const [configBackupDrive] = destinationRootParts.splice(2, 1);
+    if (destinationRootParts.join("###") !== "###Volumes###SD Card Backup###") {
+      console.error(
+        `Destination root is not supported yet: ${destinationRoot}`,
+      );
+      exit(1);
+    }
 
-const destinationRoot: string = (
-  await Bun.file(join(homedir(), "/.config/sd-card-backup/config.json")).json()
-).destination_root;
-const destinationRootParts = destinationRoot.split("/");
-const [configBackupDrive] = destinationRootParts.splice(2, 1);
-if (destinationRootParts.join("###") !== "###Volumes###SD Card Backup###") {
-  console.error(`Destination root is not supported yet: ${destinationRoot}`);
-  exit(1);
-}
+    const parts = filePath.split("/");
 
-const parts = filePath.split("/");
+    // TODO: folder mappings other than DCIM
 
-// TODO: folder mappings other than DCIM
+    if (parts[2] === configBackupDrive) {
+      if (parts.length !== 11) {
+        throw new Error("Invalid path.");
+      }
+      const [
+        root,
+        Volumes,
+        backupDrive,
+        SDCardBackup,
+        _targetClassificationFolder,
+        _year,
+        _date,
+        cardName,
+        DCIM_OR_CRM,
+        parentFolderName,
+        fileName,
+      ] = parts;
+      if (
+        root !== "" ||
+        Volumes !== "Volumes" ||
+        backupDrive !== configBackupDrive ||
+        SDCardBackup !== "SD Card Backup" ||
+        !["DCIM", "CRM"].includes(DCIM_OR_CRM)
+      ) {
+        throw new Error("Invalid path!");
+      }
+      const targetParts = [
+        "",
+        "Volumes",
+        cardName,
+        DCIM_OR_CRM,
+        parentFolderName,
+        fileName,
+      ];
 
-if (parts[2] === configBackupDrive) {
-  if (parts.length !== 11) {
-    throw new Error("Invalid path.");
-  }
-  const [
-    root,
-    Volumes,
-    backupDrive,
-    SDCardBackup,
-    _targetClassificationFolder,
-    _year,
-    _date,
-    cardName,
-    DCIM_OR_CRM,
-    parentFolderName,
-    fileName,
-  ] = parts;
-  if (
-    root !== "" ||
-    Volumes !== "Volumes" ||
-    backupDrive !== configBackupDrive ||
-    SDCardBackup !== "SD Card Backup" ||
-    !["DCIM", "CRM"].includes(DCIM_OR_CRM)
-  ) {
-    throw new Error("Invalid path!");
-  }
-  const targetParts = [
-    "",
-    "Volumes",
-    cardName,
-    DCIM_OR_CRM,
-    parentFolderName,
-    fileName,
-  ];
+      const targetPath = targetParts.join("/");
+      // console.log(targetPath);
+      await new PrintableShellCommand("/opt/homebrew/bin/reveal-macos", [
+        targetPath,
+      ]).spawn().success;
 
-  const targetPath = targetParts.join("/");
-  // console.log(targetPath);
-  await new PrintableShellCommand("/opt/homebrew/bin/reveal-macos", [
-    targetPath,
-  ]).spawn().success;
+      exit(0);
+    }
 
-  exit(0);
-}
+    if (parts.length !== 6) {
+      throw new Error("Invalid path.");
+    }
+    const [root, Volumes, cardName, DCIM_OR_CRM, parentFolderName, fileName] =
+      parts;
 
-if (parts.length !== 6) {
-  throw new Error("Invalid path.");
-}
-const [root, Volumes, cardName, DCIM_OR_CRM, parentFolderName, fileName] =
-  parts;
+    if (
+      root !== "" ||
+      Volumes !== "Volumes" ||
+      !["DCIM", "CRM"].includes(DCIM_OR_CRM)
+    ) {
+      throw new Error("Invalid path!");
+    }
 
-if (
-  root !== "" ||
-  Volumes !== "Volumes" ||
-  !["DCIM", "CRM"].includes(DCIM_OR_CRM)
-) {
-  throw new Error("Invalid path!");
-}
+    const statResult = await stat(filePath);
+    const yearString = statResult.birthtime.getFullYear().toString();
+    const monthString = (statResult.birthtime.getMonth() + 1)
+      .toString()
+      .padStart(2, "0");
+    const dayString = statResult.birthtime
+      .getDate()
+      .toString()
+      .padStart(2, "0");
+    const dateString = [yearString, monthString, dayString].join("-");
 
-const statResult = await stat(filePath);
-const yearString = statResult.birthtime.getFullYear().toString();
-const monthString = (statResult.birthtime.getMonth() + 1)
-  .toString()
-  .padStart(2, "0");
-const dayString = statResult.birthtime.getDate().toString().padStart(2, "0");
-const dateString = [yearString, monthString, dayString].join("-");
+    // TODO: turn this into a `Set`.
+    const imageExtensions = new Set([
+      ".arw",
+      ".bmp",
+      ".cr2",
+      ".cr3",
+      ".dng",
+      ".gif",
+      ".jpeg",
+      ".jpg",
+      ".nef",
+      ".png",
+      ".raw",
+      ".tif",
+      ".webm",
+    ]);
 
-// TODO: turn this into a `Set`.
-const imageExtensions = new Set([
-  ".arw",
-  ".bmp",
-  ".cr2",
-  ".cr3",
-  ".dng",
-  ".gif",
-  ".jpeg",
-  ".jpg",
-  ".nef",
-  ".png",
-  ".raw",
-  ".tif",
-  ".webm",
-]);
+    // TODO: This is currently conservative. A more robust approach using file(1) or
+    // `http.DetectContentType` would be nice, although both are hacky.
+    const videoExtensions = new Set([
+      ".avi",
+      ".m4v",
+      ".mkv",
+      ".mov",
+      ".mp4",
+      ".mpeg:",
+      ".mpg:",
+      ".mts",
+      ".mxf",
+      ".crm", // Canon raw movie
+    ]);
 
-// TODO: This is currently conservative. A more robust approach using file(1) or
-// `http.DetectContentType` would be nice, although both are hacky.
-const videoExtensions = new Set([
-  ".avi",
-  ".m4v",
-  ".mkv",
-  ".mov",
-  ".mp4",
-  ".mpeg:",
-  ".mpg:",
-  ".mts",
-  ".mxf",
-  ".crm", // Canon raw movie
-]);
+    // TODO: This is currently conservative. A more robust approach using file(1) or
+    // `http.DetectContentType` would be nice, although both are hacky.
+    const audioExtensions = new Set([
+      ".aac",
+      ".aif",
+      ".aiff",
+      ".flac",
+      ".m4a",
+      ".mp3",
+      ".ogg",
+      ".wav",
+      ".wma",
+    ]);
 
-// TODO: This is currently conservative. A more robust approach using file(1) or
-// `http.DetectContentType` would be nice, although both are hacky.
-const audioExtensions = new Set([
-  ".aac",
-  ".aif",
-  ".aiff",
-  ".flac",
-  ".m4a",
-  ".mp3",
-  ".ogg",
-  ".wav",
-  ".wma",
-]);
+    let targetClassificationFolder = "Unsorted";
+    const extension = extname(filePath).toLowerCase();
+    if (imageExtensions.has(extension)) {
+      targetClassificationFolder = "Images";
+    } else if (videoExtensions.has(extension)) {
+      if (DCIM_OR_CRM === "CRM") {
+        targetClassificationFolder = "RAW Video";
+      } else {
+        targetClassificationFolder = "Videos";
+      }
+    } else if (audioExtensions.has(extension)) {
+      targetClassificationFolder = "Audio";
+    }
 
-let targetClassificationFolder = "Unsorted";
-const extension = extname(filePath).toLowerCase();
-if (imageExtensions.has(extension)) {
-  targetClassificationFolder = "Images";
-} else if (videoExtensions.has(extension)) {
-  if (DCIM_OR_CRM === "CRM") {
-    targetClassificationFolder = "RAW Video";
-  } else {
-    targetClassificationFolder = "Videos";
-  }
-} else if (audioExtensions.has(extension)) {
-  targetClassificationFolder = "Audio";
-}
+    const targetParts = [
+      "",
+      "Volumes",
+      configBackupDrive,
+      "SD Card Backup",
+      targetClassificationFolder,
+      yearString,
+      dateString,
+      cardName,
+      DCIM_OR_CRM,
+      parentFolderName,
+      fileName,
+    ];
 
-const targetParts = [
-  "",
-  "Volumes",
-  configBackupDrive,
-  "SD Card Backup",
-  targetClassificationFolder,
-  yearString,
-  dateString,
-  cardName,
-  DCIM_OR_CRM,
-  parentFolderName,
-  fileName,
-];
+    const targetPath = targetParts.join("/");
+    // console.log(targetPath);
+    await new PrintableShellCommand("/opt/homebrew/bin/reveal-macos", [
+      targetPath,
+    ]).spawn().success;
+  },
+});
 
-const targetPath = targetParts.join("/");
-// console.log(targetPath);
-await new PrintableShellCommand("/opt/homebrew/bin/reveal-macos", [
-  targetPath,
-]).spawn().success;
+await run(binary(app), argv);

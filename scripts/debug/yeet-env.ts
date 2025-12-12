d99f3f6daf48aa805cd69721b3dbdbdd4a6b5365
Lucas Garron <code@garron.net>
2025-12-11 17:59:41 -0800

    [yeet-env.ts] Add a script to inspect env vars.

---
+++ scripts/debug/yeet-env.ts
@@ -0,0 +1,31 @@
+#!/opt/homebrew/bin/bun run --
+
+// Note: this script hard-codes paths, so that can be run from anywhere in any process.
+
+import { argv, env } from "node:process";
+import { ErgonomicDate } from "ergonomic-date";
+import { Path } from "path-class";
+import { PrintableShellCommand } from "printable-shell-command";
+
+console.error(JSON.stringify(env));
+const path = Path.xdg.cache.join(
+  "yeet-env-macOS",
+  `${new ErgonomicDate().multipurposeTimestamp}.json`,
+);
+await path.writeJSON(env);
+
+await new PrintableShellCommand("/opt/homebrew/bin/terminal-notifier", [
+  ["-title", "yeet-env-macOS"],
+  ["-message", `Env vars written to: ${path}`],
+]).shellOut();
+
+await new PrintableShellCommand(
+  "/Users/lgarron/Code/git/github.com/lgarron/dotfiles/scripts/app-tools/editor-open-file-in-workspace.ts",
+  [path],
+).shellOut();
+
+const args = argv.slice(2);
+if (args.length > 0) {
+  const [command, ...argv] = args;
+  await new PrintableShellCommand(command, argv).shellOut();
+}

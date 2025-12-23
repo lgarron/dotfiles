a6744255274db342864166424332d116f8b4e28e
Lucas Garron <code@garron.net>
2025-12-22 20:52:00 -0800

    [hevc.ts] Switch to a unified HEVC Handbrake preset.

--- scripts/video/hevc.ts
+++ scripts/video/hevc.ts
@@ -1,6 +1,5 @@
 #!/usr/bin/env -S bun run --
 
-import { exit } from "node:process";
 import {
   choice,
   integer,
@@ -13,20 +12,13 @@ import {
 import { run } from "@optique/run";
 import { ErgonomicDate } from "ergonomic-date";
 import { PrintableShellCommand } from "printable-shell-command";
-import { Temporal } from "temporal-ponyfill";
 import { byOption, fileInOut } from "../lib/optique";
-import { sleepDuration } from "../lib/temporal/sleep";
 import { ffprobeFirstVideoStream, pollOption } from "./ffpoll";
 
-const HANDBRAKE_8_BIT_DEPTH_PRESET = "HEVC 8-bit (qv65)";
-const HANDBRAKE_10_BIT_DEPTH_PRESET = "HEVC 10-bit (qv65)";
+const HANDBRAKE_PRESET = "HEVC (qv65)";
 
 const DEFAULT_ENCODER = "handbrake" as const;
 const ENCODERS = [DEFAULT_ENCODER, "ffmpeg"] as const;
-
-const HALF_SECOND = Temporal.Duration.from({ milliseconds: 500 });
-
-const FORCE_BIT_DEPTH_FLAG = "--force-bit-depth" as const;
 
 function parseArgs() {
   return run(
@@ -38,7 +30,6 @@ function parseArgs() {
         }),
         65,
       ),
-      forceBitDepth: optional(option(FORCE_BIT_DEPTH_FLAG, choice([8, 10]))),
       encoder: withDefault(
         option("--encoder", choice(ENCODERS, { metavar: "ENCODER" }), {
           description: message`Encoder binary to invoke.`,
@@ -57,90 +48,13 @@ function parseArgs() {
 }
 
 export async function hevc(args: ReturnType<typeof parseArgs>): Promise<void> {
-  const { poll, quality, forceBitDepth, height, dryRun, encoder, sourceFile } =
-    args;
+  const { poll, quality, height, dryRun, encoder, sourceFile } = args;
   const videoStream = await ffprobeFirstVideoStream({ sourceFile, poll });
 
   // const codec_fingerprint = `${videoStream.codec_name}/${videoStream.pix_fmt}/${videoStream.color_space}/${videoStream.color_primaries}/${videoStream.color_transfer}`;
   const simplifiedCodecFingerprint = `${videoStream.pix_fmt}/${videoStream.color_transfer}`;
 
   console.log(`Codec fingerprint: ${simplifiedCodecFingerprint}`);
-
-  let bitDepth: 8 | 10 = await (async () => {
-    switch (simplifiedCodecFingerprint) {
-      case "yuv422p10le/arib-std-b67":
-      case "yuv420p10le/arib-std-b67": {
-        console.log("Detected 10-bit HDR footage.");
-        return 10;
-      }
-      case "yuv420p/smpte170m":
-      case "yuv420p/bt709":
-      case "yuvj420p/bt709":
-      case "yuvj420p/iec61966-2-1": // iOS screen recordings?
-      case "yuv420p/undefined":
-      case "yuv444p/undefined": {
-        console.log("Detected 8-bit SDR (or SDR-mapped) footage.");
-        return 8;
-      }
-      case "yuv422p10le/bt709":
-      case "yuv420p10le/bt709":
-      case "yuv420p10le/undefined": {
-        if (forceBitDepth) {
-          console.log(
-            `Detected an input with 10-bit encoding for BT.709 video data. Using an output bit depth of ${forceBitDepth} from the specified ${FORCE_BIT_DEPTH_FLAG} option.`,
-          );
-          console.write("Continuing in 2 seconds");
-          for (let i = 0; i < 4; i++) {
-            if (i !== 0) {
-              console.write(".");
-            }
-            await sleepDuration(HALF_SECOND);
-          }
-          return forceBitDepth;
-        }
-        console.warn(
-          `Detected an input with 10-bit encoding for BT.709 video data. You must specify the ${FORCE_BIT_DEPTH_FLAG} option.`,
-        );
-        exit(5);
-        break; // Workaround for: https://github.com/biomejs/biome/issues/3235
-      }
-      case "yuv422p10le/undefined": {
-        console.log("Detected 10-bit SDR (or SDR-mapped) footage.");
-        return 10;
-      }
-      default: {
-        if (forceBitDepth) {
-          console.log(`Unknown simplifiedCodecFingerprint: ${simplifiedCodecFingerprint}
-
-A forced bit depth of ${forceBitDepth} was specified, and will be used.`);
-          return forceBitDepth;
-        }
-        throw new Error(
-          `Unknown simplifiedCodecFingerprint: ${simplifiedCodecFingerprint}`,
-        );
-      }
-    }
-  })();
-
-  let bitDepthFileComponent = "";
-  if (forceBitDepth) {
-    console.warn(
-      `Forced bit depth of ${forceBitDepth} was specified. Overriding…`,
-    );
-    bitDepth = forceBitDepth as 8 | 10;
-    bitDepthFileComponent = `.${forceBitDepth}-bit`;
-  }
-
-  const handbrakePreset = (() => {
-    switch (bitDepth) {
-      case 8:
-        return HANDBRAKE_8_BIT_DEPTH_PRESET;
-      case 10:
-        return HANDBRAKE_10_BIT_DEPTH_PRESET;
-      default:
-        throw new Error("Bit depth does not correspond to a HandBrake preset.");
-    }
-  })();
 
   const outputFile =
     args.outputFile ??
@@ -149,7 +63,6 @@ A forced bit depth of ${forceBitDepth} was specified, and will be used.`);
       if (encoder !== DEFAULT_ENCODER) {
         destPrefix = destPrefix.extendBasename(`.${encoder}`);
       }
-      destPrefix = destPrefix.extendBasename(`.hevc${bitDepthFileComponent}`);
       if (height) {
         destPrefix = destPrefix.extendBasename(`.${height}p`);
       }
@@ -175,7 +88,7 @@ A forced bit depth of ${forceBitDepth} was specified, and will be used.`);
             "--preset-import-file",
             "/Users/lgarron/Code/git/github.com/lgarron/dotfiles/exported/HandBrake/UserPresets.json", // TODO
           ],
-          ["--preset", handbrakePreset],
+          ["--preset", HANDBRAKE_PRESET],
           ["--quality", quality.toString()],
           ...heightParams,
           ["-i", sourceFile],

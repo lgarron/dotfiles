c7fdb1edb170641ce6613f8dbd8496ecea530c71
Lucas Garron <code@garron.net>
2025-12-26 12:01:59 -0800

    [mirror-vscode-settings-macos.ts] Switch to a unified object for the normalization config.

--- mirrored/vscode-settings-macos/mirror-vscode-settings-macos.ts
+++ mirrored/vscode-settings-macos/mirror-vscode-settings-macos.ts
@@ -12,18 +12,13 @@ const DOTFILES_SETTINGS_PATH = Path.homedir.join(
   "Code/git/github.com/lgarron/dotfiles/mirrored/vscode-settings-macos/files/Library/Application Support/Code/User/settings.jsonc",
 );
 
-const WINDOW_ZOOM_LEVEL = "window.zoomLevel";
-const WINDOW_ZOOM_LEVEL_NORMALIZED = 1;
-
-const EDITOR_FORMAT_ON_SAVE = "editor.formatOnSave";
-const EDITOR_FORMAT_ON_SAVE_NORMALIZED = true;
-
-const EDITOR_FONT_FAMILY = "editor.fontFamily";
-const EDITOR_FONT_FAMILY_NORMALIZED =
-  '"SeriousShanns Nerd Font Mono", Menlo, Monaco, monospace';
-
-const WINDOW_AUTO_DETECT_COLOR_SCHEME = "window.autoDetectColorScheme";
-const WINDOW_AUTO_DETECT_COLOR_SCHEME_NORMALIZED = false;
+const TOP_LEVEL_NORMALIZED_FIELDS = {
+  "window.zoomLevel": 1,
+  "editor.formatOnSave": true,
+  "editor.fontFamily":
+    '"SeriousShanns Nerd Font Mono", Menlo, Monaco, monospace',
+  "window.autoDetectColorScheme": false,
+};
 
 async function mirror() {
   console.log("Copying to mirror…");
@@ -32,20 +27,13 @@ async function mirror() {
       await VSCODE_SETTINGS_PATH.readText(),
     ) as CommentObject;
 
-    if (EDITOR_FORMAT_ON_SAVE in parsed) {
-      parsed[EDITOR_FORMAT_ON_SAVE] = EDITOR_FORMAT_ON_SAVE_NORMALIZED;
-    }
-
-    if (WINDOW_ZOOM_LEVEL in parsed) {
-      parsed[WINDOW_ZOOM_LEVEL] = WINDOW_ZOOM_LEVEL_NORMALIZED;
-    }
-
-    if (EDITOR_FONT_FAMILY in parsed) {
-      parsed[EDITOR_FONT_FAMILY] = EDITOR_FONT_FAMILY_NORMALIZED;
-    }
-
-    if (WINDOW_AUTO_DETECT_COLOR_SCHEME in parsed) {
-      parsed[EDITOR_FONT_FAMILY] = WINDOW_AUTO_DETECT_COLOR_SCHEME_NORMALIZED;
+    for (const [key, normalizedValue] of Object.entries(
+      TOP_LEVEL_NORMALIZED_FIELDS,
+    )) {
+      console.log(`Rewriting key: ${key}`);
+      if (key in parsed) {
+        parsed[key] = normalizedValue;
+      }
     }
 
     await DOTFILES_SETTINGS_PATH.write(stringify(parsed, null, "\t"));

c9b9f97b3218e2b06bcb2b898d3065a476da67ee
Lucas Garron <code@garron.net>
2026-01-03 18:36:33 -0800

    [launch-fish-in-dir] Pass `--login` to fish to ensure the correct `$PATH`.

--- scripts/app-tools/launch-fish-in-dir.fish
+++ scripts/app-tools/launch-fish-in-dir.fish
@@ -2,4 +2,4 @@
 
 # set PATH_UTF8 (printf %s $argv[1] | iconv -f "UTF-8" -t "ISO-8859-1")
 set PATH_BASE64 (printf %s $argv[1] | base64)
-/opt/homebrew/bin/fish --interactive --init-command "cd-dir-from-iterm-base64 "(string escape $PATH_BASE64)
+/opt/homebrew/bin/fish --login --interactive --init-command "cd-dir-from-iterm-base64 "(string escape $PATH_BASE64)

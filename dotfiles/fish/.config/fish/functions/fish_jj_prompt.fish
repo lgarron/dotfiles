7064e84432f23ea8039e5600a1a270245a429b30
Lucas Garron <code@garron.net>
2026-01-06 11:58:50 -0800

    [fish_jj_prompt.fish] Try adding the current op (truncated) to the prompt.

--- dotfiles/fish/.config/fish/functions/fish_jj_prompt.fish
+++ dotfiles/fish/.config/fish/functions/fish_jj_prompt.fish
@@ -21,7 +21,7 @@ function fish_jj_prompt
     if not command jj root &>/dev/null
         return 1
     end
-    set -l info (
+    set -l info op=(timeout --preserve-status 1s jj op log --limit 1 --no-graph --ignore-working-copy --color=always --template 'id.short().substr(0, 4)' 2>/dev/null || printf "%s" $TIMED_OUT)" "(
         timeout --preserve-status 1s jj log 2>/dev/null --no-graph --ignore-working-copy --color=always --revisions @ \
             --template '
                     separate(

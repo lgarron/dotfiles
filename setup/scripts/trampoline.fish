cf4d27515457ff6e019e9433e70ba876297f71d6
Lucas Garron <code@garron.net>
2026-01-08 22:39:33 -0800

    [trampoline] Create a formula for the trampoline.

--- setup/scripts/trampoline.fish
+++ setup/scripts/trampoline.fish
@@ -2,15 +2,12 @@
 
 # TODO: dedupe with `path.fish`
 set PATH \
-  "$HOME/.local/share/cargo/bin" \
-  /opt/homebrew/bin \
-  /home/linuxbrew/.linuxbrew/bin \
-  /home/linuxbrew/.linuxbrew/sbin \
-  "$HOME/.local/share/gopath/bin" \
-  $HOME/.cache/.bun/bin \
-  "/usr/local/bin" \
-  "$HOME/.config/binaries/linux-x64" \
-  $PATH
+    "$HOME/.local/share/cargo/bin" \
+    /opt/homebrew/bin \
+    "$HOME/.local/share/gopath/bin" \
+    $HOME/.cache/.bun/bin \
+    /usr/local/bin \
+    $PATH
 
 source /Users/lgarron/Code/git/github.com/lgarron/dotfiles/dotfiles/xdg-basedir-workarounds/.config/fish/xdg-basedir-workarounds.fish
 

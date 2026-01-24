7eaa3f773d8b0c011d5926c99899ad13e7304ce3
Lucas Garron <code@garron.net>
2026-01-24 15:25:27 -0800

    [screen-sharing] Support specifying pixel ratio and notch presence.

--- scripts/system/screen-sharing.ts
+++ scripts/system/screen-sharing.ts
@@ -26,22 +26,93 @@ const MBP_16_INCH_NOTCHLESS_RESOLUTION = "1728x1080";
 const PYTHAGORAS_LAN_HOSTNAME = "Pythagoras.lan";
 const PYTHAGORAS_TAILSCALE_HOSTNAME = "Pythagoras-ts.wyvern-climb.ts.net";
 
-const resolutionParser: ValueParser<"sync", string> = {
+// biome-ignore lint/suspicious/noExplicitAny: `any` is the correct API.
+function isPositiveInteger(n: any): n is number {
+  return Number.isInteger(n) && n > 0;
+}
+
+// biome-ignore lint/suspicious/noExplicitAny: `any` is the correct API.
+function isNotUndefined<T extends Exclude<any, undefined>>(
+  v: T | undefined,
+): v is T {
+  return typeof v !== "undefined";
+}
+
+class ResolutionInfo {
+  constructor(
+    public readonly data: {
+      width: number;
+      height: number;
+      pixelRatio?: number;
+      notch?: boolean;
+    },
+  ) {
+    if (!isPositiveInteger(data.width)) {
+      throw new Error("Invalid width (expected a positive integer).");
+    }
+    if (!isPositiveInteger(data.height)) {
+      throw new Error("Invalid height (expected a positive integer).");
+    }
+    if (
+      isNotUndefined(data.pixelRatio) &&
+      !isPositiveInteger(data.pixelRatio)
+    ) {
+      throw new Error(
+        "Invalid pixel ratio (expected a positive integer if set).",
+      );
+    }
+    if (isNotUndefined(data.notch) && typeof data.notch !== "boolean") {
+      throw new Error("Invalid notch (expected boolean if present).");
+    }
+  }
+
+  static fromString(s: string): ResolutionInfo {
+    const match = s.match(
+      /^([1-9][0-9]*)[x×]([1-9][0-9]*)+(@([1-9][0-9]*)x)?([+-]notch)?$/,
+    );
+    console.log({ s, match });
+    if (!match) {
+      throw new Error("Invalid resolution info.");
+    }
+    const width = parseInt(match[1], 10);
+    const height = parseInt(match[2], 10);
+    const pixelRatio = match[4] ? parseInt(match[4], 10) : undefined;
+    const notch = match[5] ? match[5][0] === "+" : undefined;
+    return new ResolutionInfo({ width, height, pixelRatio, notch });
+  }
+
+  logicalResolutionString(): string {
+    return `${this.data.width}x${this.data.height}`;
+  }
+
+  toString(): string {
+    let output = this.logicalResolutionString();
+    if (isNotUndefined(this.data.pixelRatio)) {
+      output += `@${this.data.pixelRatio}x`;
+    }
+    if (isNotUndefined(this.data.notch)) {
+      output += `${this.data.notch ? "+" : "-"}notch`;
+    }
+    return output;
+  }
+}
+
+const resolutionParser: ValueParser<"sync", ResolutionInfo> = {
   $mode: "sync",
   metavar: "RESOLUTION",
   parse: (input) => {
-    const match = input.match(/^([1-9][0-9]*)[x×]([1-9][0-9]*)+$/);
-    if (match) {
-      const value = `${match[1]}x${match[2]}`;
+    try {
+      const value = ResolutionInfo.fromString(input);
       return { success: true, value };
-    } else {
+    } catch (e) {
+      console.log(e);
       return {
         success: false,
         error: message`Must resemble a format like \`1920x1080\`.`,
       };
     }
   },
-  format: (value) => value,
+  format: (value) => value.logicalResolutionString(),
 };
 
 const reconnectArgs = {
@@ -181,11 +252,39 @@ export async function prep(args: SubcommandArgs<"prep">): Promise<void> {
   assert(display);
 
   const currentResolution = await display.string.get("resolution");
+  const currentHiDPI = await display.boolean.get("hiDPI");
+  // biome-ignore lint/suspicious/noExplicitAny: TODO: upstream
+  const currentNotch = await display.boolean.get("notch" as any);
 
-  if (currentResolution === args.resolution) {
+  if (currentResolution === args.resolution.logicalResolutionString()) {
     console.info(`Current resolution already matches: \`${args.resolution}\``);
   } else {
-    await display.string.set("resolution", args.resolution);
+    await display.string.set(
+      "resolution",
+      args.resolution.logicalResolutionString(),
+    );
+  }
+  const { pixelRatio } = args.resolution.data;
+  if (isNotUndefined(pixelRatio)) {
+    if (pixelRatio === (currentHiDPI ? 2 : 1)) {
+      console.info(
+        `Current pixerl ratio already matches: \`${args.resolution}\``,
+      );
+    } else {
+      if (![1, 2].includes(pixelRatio)) {
+        throw new Error(`Unsupported pixel ratio: ${pixelRatio}`);
+      }
+      await display.boolean.set("hiDPI", pixelRatio === 2);
+    }
+  }
+  const { notch } = args.resolution.data;
+  if (isNotUndefined(notch)) {
+    if (notch !== currentNotch) {
+      console.info(`Current notch already matches: \`${args.resolution}\``);
+    } else {
+      // biome-ignore lint/suspicious/noExplicitAny: TODO: upstream
+      await display.boolean.set("notch" as any, notch);
+    }
   }
 
   if (args.disconnectDisplay) {

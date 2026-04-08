b3c3da6d2b270d71f99b2641ba2095e579e28acc
Lucas Garron <code@garron.net>
2026-04-07 17:32:59 -0700

    [gclone.ts] Handle Tangled-style repo checkout.

--- scripts/git/gclone.ts
+++ scripts/git/gclone.ts
@@ -25,6 +25,7 @@ const DOT_GIT = ".git";
 const REWRITE_HOSTS: {
   [hostname: string]: {
     ssh: boolean;
+    sshPathSpec: boolean;
     staticUsername: "git";
     dotGitSuffix: boolean;
   };
@@ -32,8 +33,15 @@ const REWRITE_HOSTS: {
   // e.g. https://codeberg.org/lgarron/lockfile-mutex → `ssh://git@codeberg.org/lgarron/lockfile-mutex.git`
   "codeberg.org": {
     ssh: true,
+    sshPathSpec: false,
     staticUsername: "git",
     dotGitSuffix: true,
+  },
+  "tangled.org": {
+    ssh: true,
+    sshPathSpec: true,
+    staticUsername: "git",
+    dotGitSuffix: false,
   },
 };
 
@@ -56,6 +64,7 @@ function parseArgs() {
         true,
       ),
       url: argument(url({ allowedProtocols: ["https:"] })),
+      dryRun: option("--dry-run"),
     }),
     byOption(),
   );
@@ -63,9 +72,9 @@ function parseArgs() {
 
 function calculateRepoCloneSource(
   url: URL,
-  options: { rewriteURL: string },
+  { rewriteURL }: Pick<ReturnType<typeof parseArgs>, "rewriteURL">,
 ): {
-  repoCloneSourceURL: string;
+  repoCloneSource: string;
   user: string;
   repo: string;
 } {
@@ -77,9 +86,25 @@ function calculateRepoCloneSource(
   }
 
   const rewrite = REWRITE_HOSTS[url.hostname];
-  if (!rewrite || !options.rewriteURL) {
+  if (!rewrite || !rewriteURL) {
     return {
-      repoCloneSourceURL: new URL(join("/", user, repo), url).toString(),
+      repoCloneSource: new URL(join("/", user, repo), url).toString(),
+      user: user,
+      repo,
+    };
+  }
+  assert.equal(rewrite.staticUsername, "git");
+  const sshUsername = rewrite.staticUsername;
+
+  // TODO: handle edge cases when the repo name itself contains `.git`?
+  let path = new Path(user).join(repo);
+  if (rewrite.dotGitSuffix && !path.path.endsWith(DOT_GIT)) {
+    path = path.extendBasename(DOT_GIT);
+  }
+
+  if (rewrite.sshPathSpec) {
+    return {
+      repoCloneSource: `${sshUsername}@${url.hostname}:${user}/${repo}`,
       user,
       repo,
     };
@@ -92,23 +117,21 @@ function calculateRepoCloneSource(
   // > If the URL has no host or its scheme is file:, then setting this property has no effect.
   //
   // https://developer.mozilla.org/en-US/docs/Web/API/URL/username
-  assert.equal(rewrite.staticUsername, "git");
-  newURL.username = rewrite.staticUsername;
-  // TODO: handle edge cases when the repo name itself contains `.git`?
-  let path = new Path(user).join(repo);
-  if (rewrite.dotGitSuffix && !path.path.endsWith(DOT_GIT)) {
-    path = path.extendBasename(DOT_GIT);
-  }
+  newURL.username = sshUsername;
   newURL.pathname = path.path;
-  console.log(newURL.toString());
-  return { repoCloneSourceURL: newURL.toString(), user, repo };
+  return { repoCloneSource: newURL.toString(), user, repo };
 }
 
 export async function gclone({
   rewriteURL,
   url,
+  dryRun,
 }: ReturnType<typeof parseArgs>): Promise<void> {
-  const { repoCloneSourceURL, user, repo } = calculateRepoCloneSource(url, {
+  const {
+    repoCloneSource: repoCloneSource,
+    user,
+    repo,
+  } = calculateRepoCloneSource(url, {
     rewriteURL,
   });
   const repoPathParent = GIT_REPOS_ROOT_PATH.join(url.hostname, user);
@@ -123,36 +146,44 @@ export async function gclone({
     console.log(repoPath.path);
   } else {
     await repoPathParent.mkdir();
-    console.log(`Cloning from: ${repoCloneSourceURL}`);
+    console.log(`Cloning from: ${repoCloneSource}`);
     console.log(`To: ${repoPath}`);
 
     const DATA_DIR = Path.xdg.data.join("gclone");
     await DATA_DIR.mkdir();
 
     // TODO: logging?
-    new PrintableShellCommand("git", [
+    const gitCommand = new PrintableShellCommand("git", [
       "clone",
-      repoCloneSourceURL,
+      repoCloneSource,
       repoPath,
-    ]).spawnDetached({});
+    ]);
+    if (dryRun) {
+      console.log("Skipping the following `git` command due to dry run:");
+      gitCommand.print();
+    } else {
+      gitCommand.print().spawnDetached({});
 
-    // `git` insists on creating instead of inheriting a folder. We could clone
-    // the repo in another folder and move the `.git` repo to the correct place,
-    // but this causes other issues. So we wait for `git` to create the folder.
-    const start = monotonicNow();
-    await (async () => {
-      while (monotonicNow().since(start).total({ unit: "seconds" }) < 5) {
-        if (await repoPath.exists()) {
-          return;
+      // `git` insists on creating instead of inheriting a folder. We could clone
+      // the repo in another folder and move the `.git` repo to the correct place,
+      // but this causes other issues. So we wait for `git` to create the folder.
+      const start = monotonicNow();
+      await (async () => {
+        while (monotonicNow().since(start).total({ unit: "seconds" }) < 5) {
+          if (await repoPath.exists()) {
+            return;
+          }
+          await sleepDuration(Temporal.Duration.from({ milliseconds: 100 }));
         }
-        await sleepDuration(Temporal.Duration.from({ milliseconds: 100 }));
-      }
-      throw new Error("Did not observe `git` creating the repo folder.");
-    })();
+        throw new Error("Did not observe `git` creating the repo folder.");
+      })();
+    }
   }
 
-  await new PrintableShellCommand("open-macos", [repoPath]).spawn().success;
-  await new PrintableShellCommand("code", [repoPath]).spawn().success;
+  if (!dryRun) {
+    await new PrintableShellCommand("open-macos", [repoPath]).spawn().success;
+    await new PrintableShellCommand("code", [repoPath]).spawn().success;
+  }
 }
 
 if (import.meta.main) {

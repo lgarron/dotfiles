8da50e96535c16024ce1cd488044da6fedce7410
Lucas Garron <code@garron.net>
2026-06-19 03:09:13 -0700

    [install-homebrew.bash] Spell out trust for now.

--- setup/scripts/install-homebrew.bash
+++ setup/scripts/install-homebrew.bash
@@ -13,7 +13,16 @@ echo "Installing Homebrew…"
 # https://brew.sh/
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 
+# TODO: move this into Brewfiles
 /opt/homebrew/bin/brew tap lgarron/lgarron
 /opt/homebrew/bin/brew trust lgarron/lgarron
 /opt/homebrew/bin/brew tap oven-sh/bun
 /opt/homebrew/bin/brew trust --formula oven-sh/bun/bun
+/opt/homebrew/bin/brew tap cubing/cubing
+/opt/homebrew/bin/brew trust cubing/cubing
+/opt/homebrew/bin/brew tap teamookla/speedtest
+/opt/homebrew/bin/brew trust --formula teamookla/speedtest/speedtest
+/opt/homebrew/bin/brew tap jurplel/tap
+/opt/homebrew/bin/brew trust --cask jurplel/tap/instant-space-switcher
+/opt/homebrew/bin/brew tap waydabber/betterdisplay
+/opt/homebrew/bin/brew trust --formula waydabber/betterdisplay/betterdisplaycli
